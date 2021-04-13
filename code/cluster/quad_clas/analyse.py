33e3a4a18621421f2467ea3a50e58152830cee89
Jaco ter Hoeve <j.j.ter.hoeve@vu.nl>
2021-04-13 09:06:11 +0200

    backup before cleaning up code
    Former-commit-id: 05eb4f74745fc3abd8fc5b3ccc4726f995bf0b11 [formerly 881d2aa8be036823f216e111563571152b33a7eb]
    Former-commit-id: 7eba63130a019426f5350da7ca74d4d1f61df311

--- code/cluster/quad_clas/analyse.py
+++ code/cluster/quad_clas/analyse.py
@@ -13,7 +13,7 @@ from quad_classifier_cluster import Predictor_quadratic
 import xsec_cluster as ExS
 
 matplotlib.use('PDF')
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'], 'size': 22})
 rc('text', usetex=True)
 
 eft_points = [[-10.0, 0], [-5.0, 0], [-1.0, 0], [1.0, 0], [5.0, 0], [10.0, 0], [0, -2.0], [0, -1.0], [0, -0.5],[0, 0.5], [0, 1.0], [0, 2.0], [-10.0, -2.0], [-5.0, -1.0], [-1.0, -0.5], [1.0, 0.5], [5.0, 1.0],[10.0, 2.0]]
@@ -133,17 +133,17 @@ def plot_predictions_1d(network_size):
             if j == 1: #data versus theory
                 ax = fig.add_subplot(inner[-1, :])
 
-                #ax.plot(x * 1e3, (y - f_pred_median_2) / f_pred_std_2)
-                ax.plot(x * 1e3, y / f_pred_median_1, color='C0')
-                ax.plot(x * 1e3, y / f_pred_median_2, color='C1')
-                ax.hlines(1, mtt_min, mtt_max, linestyles='dashed', colors='black')
+                ax.plot(x * 1e3, (y - f_pred_median_2) / f_pred_std_2)
+                #ax.plot(x * 1e3, y / f_pred_median_1, color='C0')
+                #ax.plot(x * 1e3, y / f_pred_median_2, color='C1')
+                #ax.hlines(1, mtt_min, mtt_max, linestyles='dashed', colors='black')
                 plt.xlabel(r'$m_{tt}\;[\mathrm{GeV}]$')
-                plt.ylabel(r'$\rm{theory/model}$')
-                #plt.ylabel(r'$\rm{pull}$')
+                #plt.ylabel(r'$\rm{theory/model}$')
+                plt.ylabel(r'$\rm{pull}$')
                 plt.xlim((mtt_min, mtt_max))
-                plt.ylim((0.9*(y / f_pred_median_1).min(), 1.1*(y / f_pred_median_1).max()))
-                #plt.ylim((1.2 * np.min((y - f_pred_median_1) / f_pred_std_2),
-                #          1.2 * np.max((y - f_pred_median_1) / f_pred_std_2)))
+                #plt.ylim((0.9*(y / f_pred_median_1).min(), 1.1*(y / f_pred_median_1).max()))
+                plt.ylim((1.2 * np.min((y - f_pred_median_2) / f_pred_std_2),
+                          1.2 * np.max((y - f_pred_median_2) / f_pred_std_2)))
                 # fig.savefig('/data/theorie/jthoeve/ML4EFT/quad_clas/qc_results/' + 'pull_v2.pdf')
 
 
@@ -159,8 +159,84 @@ def plot_predictions_1d(network_size):
     #     if i%6 == 0:
     #         pdf.savefig(fig)
     # pdf.close()
-    fig.savefig('/data/theorie/jthoeve/ML4EFT/quad_clas/qc_results/' + 'test.pdf')
+    fig.savefig('/data/theorie/jthoeve/ML4EFT/quad_clas/qc_results/' + 'test_pull.pdf')
 
+
+def get_predictions_1d(network_path, network_size, mtt, ctg, cuu, mean, std):
+    loaded_model = Predictor_quadratic(network_size)
+    loaded_model.load_state_dict(torch.load(network_path))
+    x = (mtt * 10**3 - mean) / std  # rescale the inputs
+    x = torch.tensor([x])
+    f_pred = loaded_model.forward(x.float(), ctg, cuu)
+    f_pred = f_pred.detach().numpy()
+    return f_pred
+
+
+def get_pull(network_size, ctg, cuu, mtt):
+    mc_runs = 42
+
+    f_pred = []
+    for i in range(1, mc_runs + 1):
+        pred_path = '/data/theorie/jthoeve/ML4EFT/quad_clas/qc_results/trained_nn/run_8/mc_run_{}'.format(i)
+        mean, std = np.loadtxt(pred_path + '/scaling.dat')
+        f_pred_i = get_predictions_1d(pred_path + '/trained_nn.pt', network_size, mtt, ctg, cuu, mean, std)
+        f_pred.append(f_pred_i)
+
+    f_pred = np.array(f_pred)
+    f_pred_median = np.median(f_pred, axis=0)
+    f_pred_std = np.std(f_pred, axis=0)
+
+    theory = 1 / (1 + ExS.likelihood_ratio_1D_v(mtt, ctg.numpy(), cuu.numpy()))
+    pull = (theory - f_pred_median)/f_pred_std
+    return pull
+
+
+def plot_pull_heatmap(network_size,mtt):
+    #plt.figure()
+
+
+    ctg = np.arange(-10, 10, 0.5)
+    cuu = np.arange(-2, 2, 0.1)
+    # ctg = np.array([-2, -1, 1, 2])
+    # cuu = np.array([-2, -1, 1, 2])
+    ctg_grid, cuu_grid = np.meshgrid(ctg, cuu, indexing='xy')
+    # pull = get_pull(network_size, torch.from_numpy(ctg_grid), torch.from_numpy(cuu_grid), mtt)
+
+
+    #fig = plt.figure()
+    #plt.imshow(pull, extent=[np.min(ctg), np.max(ctg), np.min(cuu), np.max(cuu)], origin='lower', cmap=plt.cm.summer,
+    #           aspect=(np.max(ctg) - np.min(ctg)) / (cuu.max() - cuu.min()), interpolation='quadric')
+    #plt.colorbar()
+    #plt.xlabel(r'$\rm{ctg}$')
+    #plt.ylabel(r'$\rm{cuu}$')
+    #plt.title(r'$\rm{Pull\:heatmap\:at\:m_{tt}=\:}$' + r'${}$'.format(mtt) + r'$\:\mathrm{TeV}$')
+    # fig.savefig('/data/theorie/jthoeve/ML4EFT/quad_clas/qc_results/pull_heatmap.pdf')
+
+
+    #################
+
+    nrows = 3
+    ncols = 2
+
+
+    fig = plt.figure(figsize=(ncols * 10, nrows * 10))
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    for i in range(1, nrows*ncols+1):
+        pull = get_pull(network_size, torch.from_numpy(ctg_grid), torch.from_numpy(cuu_grid), mtt[i-1])
+        ax = fig.add_subplot(nrows, ncols, i)
+        im = ax.imshow(pull, extent=[np.min(ctg), np.max(ctg), np.min(cuu), np.max(cuu)], origin='lower', cmap='RdBu', aspect=(np.max(ctg) - np.min(ctg)) / (cuu.max() - cuu.min()), interpolation='quadric', vmin=-1.0, vmax=1.0)
+        cbar = fig.colorbar(im, ax=ax, extend='both', shrink=0.60)
+        cbar.minorticks_on()
+
+
+        #ax.colorbar()'
+        #fig.colorbar(im, ax=ax)
+        ax.set_xlabel(r'$\rm{ctg}$')
+        ax.set_ylabel(r'$\rm{cuu}$')
+        ax.set_title(r'$\rm{Pull\:at\:m_{tt}=\:}$' + r'${}$'.format(mtt[i-1]) + r'$\:\mathrm{TeV}$')
+        fig.tight_layout()
+
+    fig.savefig('/data/theorie/jthoeve/ML4EFT/quad_clas/qc_results/pull_heatmap_v3.pdf')
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as json_data:
@@ -174,9 +250,10 @@ if __name__ == '__main__':
     cuu = run_dict['coeff'][1]['value']
     network_size = [run_dict['input_size']] + run_dict['hidden_sizes'] + [run_dict['output_size']]
 
+    #pull = get_pull(network_size, torch.tensor([0]), torch.tensor([1]))
+    #print(pull)
+    plot_pull_heatmap(network_size, [1.50, 1.80, 2.10, 2.40, 3.00, 3.50])
 
 
 
-
-
-    plot_predictions_1d(network_size)
+    #plot_predictions_1d(network_size)
