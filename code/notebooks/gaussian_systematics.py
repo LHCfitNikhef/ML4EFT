9e2f3eb8b45a05e03bf547309e2696f2e9c22741
jacoterh <54140851+jacoterh@users.noreply.github.com>
2021-10-27 11:24:32 +0200

    gaussian systematics plot finished
    Former-commit-id: 7e6432d0f89393bad757f1f3807132871b7872db [formerly b74f5527b67db7c1c62b72cbd634fdc9a040671f]
    Former-commit-id: 981a2a823b1f54c009041bff47060b91cbb3d31f

--- code/notebooks/gaussian_systematics.py
+++ code/notebooks/gaussian_systematics.py
@@ -19,7 +19,7 @@ nu_i_eft = vh_prod.nu_i(coeff, cHW, cHq3, lumi, lin=True)
 
 # pseudo data settings
 delta_stat = 0.05
-delta_lum = 0.02
+delta_lum = 0.1
 
 r_i_stat = np.random.normal(size=n_dat)
 r_sys = np.random.normal()
@@ -43,9 +43,10 @@ def chi2(data, coeff, cHW, cHq3, r_sys, beta_i, alpha_i):
     chi2_value = np.sum((data-theory-r_sys * beta_i) ** 2 / alpha_i ** 2) + r_sys ** 2
     return chi2_value
 
+
 chi2_values = []
-cHW_range = np.linspace(cHW-0.2, cHW+0.2, 100)
-r_i_sys_range = np.linspace(r_sys - 4, r_sys + 4, 100)
+cHW_range = np.linspace(cHW - 0.5, cHW + 0.5, 200)
+r_i_sys_range = np.linspace(r_sys - 4, r_sys + 4, 200)
 coeff = vh_prod.findCoeff(bins)
 for cHW_i in cHW_range:
     for r_sys_i in r_i_sys_range:
@@ -53,23 +54,49 @@ for cHW_i in cHW_range:
         chi2_values.append(chi2_value)
 chi2_values = np.array(chi2_values)
 chi2_values = np.reshape(chi2_values, (cHW_range.shape[0], r_i_sys_range.shape[0]))
+
+r_i_sys_rec_array = np.array([r_i_sys_rec(cHW_i) for cHW_i in cHW_range])
+
 xx, yy = np.meshgrid(r_i_sys_range, cHW_range)
 contours = plt.contour(xx, yy, chi2_values, np.array([np.min(chi2_values)+3.84]), colors='black')
-img = plt.imshow(chi2_values, extent=[np.min(r_i_sys_range), np.max(r_i_sys_range), np.min(cHW_range), np.max(cHW_range)], origin='lower', cmap='RdGy', aspect='auto', vmin=0)
-cbar = plt.colorbar(img, shrink=0.8, extend='max')
+contour_data = np.array(contours.allsegs[0][0])
+cHW_max = np.max(contour_data, axis=0)[1]
+cHW_max_idx = np.argwhere(cHW_range >= cHW_max)[0,0]
+
+#cHW_min_idx = np.argwhere(r_i_sys_rec_array > lambda_min)[0]
+#plt.vlines(r_i_sys_range[lambda_min_idx], np.min(cHW_range), np.max(cHW_range), linestyles='dotted', color='black')
+plt.hlines(cHW_max, np.min(r_i_sys_range), np.max(r_i_sys_range), linestyles='dotted', color='black')
+plt.vlines(r_i_sys_rec_array[cHW_max_idx], np.min(cHW_range), np.max(cHW_range), linestyles='dotted', color='black')
+
+
+plt.plot(r_i_sys_rec_array,cHW_range,  linestyle='dashed', color='black')
+
 
 plt.scatter(r_sys, cHW, marker='x', color='black', label='Truth')
 idx = np.where(chi2_values == np.min(chi2_values))
 plt.scatter(r_i_sys_range[idx[1]], cHW_range[idx[0]], marker='o', color='black', label=r'$\chi^2_{\rm{min}}$')
 
-r_i_sys_rec_array = np.array([r_i_sys_rec(cHW_i) for cHW_i in cHW_range])
-plt.plot(r_i_sys_rec_array,cHW_range,  linestyle='dashed', color='black')
+img = plt.imshow(chi2_values, extent=[r_i_sys_range[idx[1]][0] - 3, r_i_sys_range[idx[1]][0] + 3, cHW_range[idx[0]][0]-0.2, cHW_range[idx[0]][0]+0.2], origin='lower', cmap='RdGy', aspect='auto', vmin=0)
+cbar = plt.colorbar(img, extend='max')
+
+plt.xlim(r_i_sys_range[idx[1]] - 3, r_i_sys_range[idx[1]] + 3)
+plt.ylim(cHW_range[idx[0]]-0.2, cHW_range[idx[0]]+0.2)
+
 plt.legend()
 #plt.vlines(r_i_sys_rec, np.min(cHW_range), np.max(cHW_range), linestyles='dashed', color='black')
 
 plt.xlabel(r'$\lambda$')
 plt.ylabel(r'$cHW$')
 plt.show()
+
+# plot chi2 profile at lambda_min
+#fig, ax = plt.subplots(figsize=(10,6))
+plt.plot(r_i_sys_range, chi2_values[cHW_max_idx,:])
+plt.axvline(r_i_sys_rec_array[cHW_max_idx],0,1, linestyle='dotted', color='black' )
+plt.xlabel(r'$\lambda$')
+plt.ylabel(r'$\chi^2$')
+plt.show()
+
 
 # plot theory predictions
 fig, ax = plt.subplots(figsize=(10,6))
