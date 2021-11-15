8b9de391b1dc0310bcdff116aa20bc6bdeaf2c75
jacoterh <54140851+jacoterh@users.noreply.github.com>
2021-11-15 17:45:58 +0100

    truth vs nn 2d heatmap of linear coefficients
    Former-commit-id: ef70b532982e6a76aecc7548bfbe876ec1614278 [formerly 4fc86f34b738a50a28c33cf44963f7d9542389be]
    Former-commit-id: fb93f569a04e8999957dcdba12d1fe9b4493dcd7

--- code/higgs21/post_analysis/2d_nn_performance.py
+++ code/higgs21/post_analysis/2d_nn_performance.py
@@ -8,7 +8,27 @@ import quad_clas.core.xsec.vh_prod as vh_prod
 import quad_clas.core.quad_classifier_cluster as quad_classifier_cluster
 import quad_clas.core.nn_analyse as analysis
 
-cmap = copy.copy(plt.get_cmap("seismic"))
+
+def plot_heatmap(im, xlabel, ylabel, title, extent, cmap='GnBu', vmin=None, vmax=None):
+
+    cmap = copy.copy(plt.get_cmap(cmap))
+    cmap.set_bad(color='white')
+
+    fig, ax = plt.subplots(figsize=(8, 7))
+    heatmap = ax.imshow(im, extent=extent,
+                   origin='lower', cmap=cmap, aspect=(extent[1] - extent[0]) / (extent[-1] - extent[-2]),
+                   interpolation='quadric', vmin=vmin, vmax=vmax)
+
+    if vmin == 0:
+        cbar = fig.colorbar(heatmap, ax=ax, shrink=0.9, extend='max')
+    else:
+        cbar = fig.colorbar(heatmap, ax=ax, shrink=0.9, extend='both')
+    cbar.minorticks_on()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    return fig
+
 
 def coeff_comp():
 
@@ -22,46 +42,93 @@ def coeff_comp():
 
     mvh_grid, y_grid = np.meshgrid(mvh_span, y_span)
     grid = np.c_[mvh_grid.ravel(), y_grid.ravel()]
+    grid_unscaled_tensor = torch.Tensor(grid)
 
+    # truth
     ratio_truth_c1 = analysis.likelihood_ratio_truth(grid, np.array([10, 0]), lin=True)
     ratio_truth_c2 = analysis.likelihood_ratio_truth(grid, np.array([0, 10]), lin=True)
     mask = ratio_truth_c1 == 0
 
-    n_alpha = np.ma.masked_where(mask, (1 - ratio_truth_c1) / 10)
-    n_beta = np.ma.masked_where(mask, (1 - ratio_truth_c2) / 10)
+    n_alpha_truth = np.ma.masked_where(mask, (ratio_truth_c1 - 1) / 10)
+    n_alpha_truth = n_alpha_truth.reshape(mvh_grid.shape)
 
-    cmap = copy.copy(plt.get_cmap("GnBu"))
-    cmap.set_bad(color='white')
+    n_beta_truth = np.ma.masked_where(mask, (ratio_truth_c2 - 1) / 10)
+    n_beta_truth = n_beta_truth.reshape(mvh_grid.shape)
 
-    # n_alpha
-    fig, ax = plt.subplots(figsize=(8, 7))
-    im = ax.imshow(n_alpha.reshape(xx.shape), extent=[mvh_min, mvh_max, y_min, y_max],
-                         origin='lower', cmap=cmap, aspect=(mvh_max - mvh_min) / (y_max - y_min),
-                         interpolation='quadric')
+    # models
 
-    cbar = fig.colorbar(im, ax=ax, shrink=0.9)
-    cbar.minorticks_on()
-    plt.xlabel(r'$m_{ZH}\;\rm{[TeV]}$')
-    plt.ylabel(r'$\rm{Rapidity}$')
-    plt.title(r'$n_{\alpha}$')
-    plt.show()
+    network_size = [2, 30, 30, 30, 30, 30, 1]
+    #model_dir = '/data/theorie/jthoeve/ML4EFT_higgs/models/model_cHq3_lin_v2/mc_run_{mc_run}'
+    model_dir = '/data/theorie/jthoeve/ML4EFT_higgs/models/model_test/mc_run_{mc_run}'
 
-    # n_beta
-    fig, ax = plt.subplots(figsize=(8, 7))
-    im = ax.imshow(n_beta.reshape(xx.shape), extent=[mvh_min, mvh_max, y_min, y_max],
-                   origin='lower', cmap=cmap, aspect=(mvh_max - mvh_min) / (y_max - y_min),
-                   interpolation='quadric')
+    n_betas = []
+    mc_runs = 30
+    for rep_nr in range(0, mc_runs):
+        print(rep_nr)
+        mean, std = np.loadtxt(os.path.join(model_dir.format(mc_run=rep_nr), 'scaling.dat'))
+        loaded_model = quad_classifier_cluster.PredictorLinear(network_size)
+        network_path = os.path.join(model_dir.format(mc_run=rep_nr), 'trained_nn.pt')
 
-    cbar = fig.colorbar(im, ax=ax, shrink=0.9)
-    cbar.minorticks_on()
-    plt.xlabel(r'$m_{ZH}\;\rm{[TeV]}$')
-    plt.ylabel(r'$\rm{Rapidity}$')
-    plt.title(r'$n_{\beta}$')
+        # load all the parameters into the trained network
+        loaded_model.load_state_dict(torch.load(network_path))
+        grid = (grid_unscaled_tensor - mean) / std
 
-    plt.show()
+        n_beta = loaded_model.n_alpha(grid.float())
+        n_beta = n_beta.view(mvh_grid.shape).detach().numpy()
+        if np.sum(n_beta) == 0:
+            continue
+        n_betas.append(n_beta)
+
+    n_betas = np.array(n_betas)
+    n_beta_median = np.percentile(n_betas, 50, axis=0)
+    n_beta_high = np.percentile(n_betas, 84, axis=0)
+    n_beta_low = np.percentile(n_betas, 16, axis=0)
+    n_beta_unc = (n_beta_high - n_beta_low) / 2
+
+    fig1 = plot_heatmap(n_alpha_truth/n_beta_median,
+                        xlabel=r'$m_{ZH}\;\rm{[TeV]}$',
+                        ylabel=r'$\rm{Rapidity}$',
+                        title=r'$n_1^{\rm{truth}}/n_1^{\rm{NN}}\;\rm{(median)}$',
+                        extent=[mvh_min, mvh_max, y_min, y_max],
+                        cmap='seismic',
+                        vmin=0.95,
+                        vmax=1.05)
+
+    fig1.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/15_11/n_alpha_ratio.pdf')
+
+    fig2 = plot_heatmap(np.abs((n_alpha_truth - n_beta_median) / n_beta_unc),
+                        xlabel=r'$m_{ZH}\;\rm{[TeV]}$',
+                        ylabel=r'$\rm{Rapidity}$',
+                        title=r'$\rm{Pull}$',
+                        extent=[mvh_min, mvh_max, y_min, y_max],
+                        vmin=0,
+                        vmax=2,
+                        cmap='seismic')
+
+    fig2.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/15_11/n_alpha_ratio_pull.pdf')
+
+    import pdb
+    pdb.set_trace()
+
+    fig2 = plot_heatmap(n_beta,
+                 xlabel=r'$m_{ZH}\;\rm{[TeV]}$',
+                 ylabel=r'$\rm{Rapidity}$',
+                 title=r'$n_{\beta}$')
+
+    fig2.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/15_11/nbeta.pdf')
+
+    fig3 = plot_heatmap(n_alpha_median,
+                        xlabel=r'$m_{ZH}\;\rm{[TeV]}$',
+                        ylabel=r'$\rm{Rapidity}$',
+                        title=r'$n_{\alpha}\;\rm{(NN)}$')
+
+    fig3.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/15_11/nalpha_nn.pdf')
+
+
 
 coeff_comp()
-
+import pdb
+pdb.set_trace()
 #%%
 
 def likelihood_ratio(y, mvh, c, lin, quad):
