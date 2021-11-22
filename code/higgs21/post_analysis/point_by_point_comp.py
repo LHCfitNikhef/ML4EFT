8249316053707569103137d8600e291a08e3daf7
jacoterh <54140851+jacoterh@users.noreply.github.com>
2021-11-22 09:28:06 +0100

    backup commit before big clean up of old state branch
    Former-commit-id: 911a7cf045d45c2943377d6234f3aece2bb6c251 [formerly 6748b5fd0c501f83ace151f20739be91f1747a96]
    Former-commit-id: 43556fe1b63b3122edfebdb86cd445afb333c8b9

--- code/higgs21/post_analysis/point_by_point_comp.py
+++ code/higgs21/post_analysis/point_by_point_comp.py
@@ -11,23 +11,25 @@ path_dict_sm = {0: path_sm_data}
 data_sm = quad_classifier_cluster.EventDataset(c=0,
                                                n_features=2,
                                                path_dict=path_dict_sm,
-                                               n_dat=500,
+                                               n_dat=1000,
                                                hypothesis=1)
 
 sm_events = data_sm.events.numpy()
 
-path_to_models = {'lin': ['/data/theorie/jthoeve/ML4EFT_higgs/models/17_11/model_final_lin_cHW/']}
+#path_to_models = {'lin': ['/data/theorie/jthoeve/ML4EFT_higgs/models/17_11/model_final_lin_cHW/']}
+path_to_models = {'lin': ['/data/theorie/jthoeve/ML4EFT_higgs/models/17_11/model_final_lin_cHW/',
+                          '/data/theorie/jthoeve/ML4EFT_higgs/models/17_11/model_final_lin_cHq3_v7/']}
 
 r_nn = []
 
 n_models = 30
 for mc_run in range(n_models):
-    r_nn_rep = likelihood_ratio_nn(torch.Tensor(sm_events), [2, 0], path_to_models, network_size, mc_run=mc_run, lin=True)
+    r_nn_rep = likelihood_ratio_nn(torch.Tensor(sm_events), [0, 2], path_to_models, network_size, mc_run=mc_run, lin=True)
     r_nn_rep = r_nn_rep.numpy().flatten()
     r_nn.append(r_nn_rep)
 tau_nn = np.log(r_nn)
 
-r_truth = likelihood_ratio_truth(sm_events, [2, 0], lin=True)
+r_truth = likelihood_ratio_truth(sm_events, [0, 2], lin=True)
 tau_truth = np.log(r_truth)
 
 # overview plot for all replicas
@@ -44,7 +46,7 @@ for i in range(ncols * nrows):
     plt.ylim((np.min(x), np.max(x)))
 
 plt.tight_layout()
-plt.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/18_11/point_by_point_cHW_2_all.pdf')
+plt.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/18_11/point_by_point_cHq3_2_all_v2.pdf')
 
 # median
 fig, ax = plt.subplots(figsize=(8, 8))
@@ -56,7 +58,7 @@ plt.ylabel(r'$\tau(x, c)^{\rm{NN}}$')
 plt.xlim((np.min(x), np.max(x)))
 plt.ylim((np.min(x), np.max(x)))
 plt.tight_layout()
-plt.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/18_11/point_by_point_cHW_2_median.pdf')
+plt.savefig('/data/theorie/jthoeve/ML4EFT_higgs/plots/18_11/point_by_point_cHq3_2_median_v2.pdf')
 
 
 
