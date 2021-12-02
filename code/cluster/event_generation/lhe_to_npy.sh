a42d5c528ebc72ac476a3cd918a8b24a682d4069
jacoterh <54140851+jacoterh@users.noreply.github.com>
2021-12-02 14:43:20 +0100

    adding instruction show to convert lhe to npy
    Former-commit-id: d43d8e6ef70a4df595bd6da3b40d2ee7beccb64e [formerly 00b27083472ea3d917ca822286880396957b1818]
    Former-commit-id: ab2cacf2d1efec785e199fe58cf8473c2a4c9d33

--- code/cluster/event_generation/lhe_to_npy.sh
+++ code/cluster/event_generation/lhe_to_npy.sh
@@ -4,28 +4,14 @@
 # Script to convert lhe files to npy extension #
 ################################################
 
-EVENT_DIR=/data/theorie/jthoeve/event_generation/lin_cHW/Events
-SAVE_LOCATION=/data/theorie/jthoeve/event_generation/events_high_stats/lin/cHW
-MCREPS=30
+EVENT_DIR=/data/theorie/jthoeve/event_generation/quad_only_cHq3/Events
+SAVE_LOCATION=/data/theorie/jthoeve/event_generation/events_high_stats/quad_only/cHq3
+MCREPS=1
 
 for ((i=0; i < $MCREPS; i++)); do
   EVENT_FILE=$EVENT_DIR/run_01_$i
-  #gunzip $EVENT_FILE/unweighted_events.lhe.gz
+  gunzip $EVENT_FILE/unweighted_events.lhe.gz
   python /data/theorie/jthoeve/ML4EFT_higgs/code/src/quad_clas/preproc/compute_events.py $EVENT_FILE/unweighted_events.lhe $SAVE_LOCATION $i
 done
 
-
-#declare -a arr=("sm" "lin" "quad" "both")
-#
-#function create_dir() {
-#
-#  for i in "${arr[@]}"; do
-#    #mkdir -p $SAVE_LOCATION/$i
-#    for ((i=1; i <= $MCREPS; i++)); do
-#      EVENT_DIR=$EVENT_DIR_ROOT/$b[]
-#      gunzip run_0$i/unweighted_events.lhe.gz
-#      #python /Users/jaco/Documents/ML4EFT/code/src/quad_clas/preproc/compute_events.py $EVENT_DIR/run_0$i/unweighted_events.lhe $SAVE_LOCATION $i
-#    done
-#  done
-#}
 
--- code/cluster/event_generation/readme.md
+++ code/cluster/event_generation/readme.md
@@ -8,3 +8,6 @@ nohup python <path to mg5_aMC> <path to runcard> &
 Example runcards for VH production are provided [here](https://github.com/LHCfitNikhef/ML4EFT/tree/classifier_quad/code/cluster/event_generation/mg5_runcards).
 
 * In case of multiple runcards we a script is provided [here](https://github.com/LHCfitNikhef/ML4EFT/tree/classifier_quad/code/cluster/event_generation/generate_events.sh) that submits jobs for each runcard in parallel.
+
+* The lhe to npy converter lets one read in the events directly as a numpy array as opposed to lhe events. This is done for speed up purposes - reading in a lhe file and computing the kinematics for each event is too costly to on the fly.
+The converter can be found [here](https://github.com/LHCfitNikhef/ML4EFT/blob/classifier_quad/code/cluster/event_generation/lhe_to_npy.sh)
