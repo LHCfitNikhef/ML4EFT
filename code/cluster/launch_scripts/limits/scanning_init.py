541d72acdf7dc538c7d8350bdf647c54725edf0e
jacoterh <54140851+jacoterh@users.noreply.github.com>
2022-02-01 23:39:00 +0100

    adding old mg5 launch scripts
    Former-commit-id: 08a9dda5bb046b96914ba56b0526e6465d6d9453 [formerly d1caef6543c4c9c527f59a5293ebeaf371f5236f]
    Former-commit-id: 319941a6d98d3ad5506d83e3b21c1b7a7750845f

--- code/cluster/launch_scripts/limits/scanning_init.py
+++ code/cluster/launch_scripts/limits/scanning_init.py
@@ -1,11 +1,14 @@
 import numpy as np
 import quad_clas.analyse.confidence_intervals as conf_int
+import sys
+
+row = sys.argv[1]
 
 # Network's architecture of the pretrained models
 architecture = [3, 30, 30, 30, 30, 30, 1]
 
-path_to_models = {'lin': ['/data/theorie/jthoeve/ML4EFT_higgs/models/2022/24_01/model_lin_cHW_3_feat',
-                          '/data/theorie/jthoeve/ML4EFT_higgs/models/2022/24_01/model_lin_cHq3_3_feat']}
+path_to_models = {'lin': ['/data/theorie/jthoeve/ML4EFT_higgs/models/2022/24_01/model_lin_cHW_3_feat/mc_run_{mc_run}',
+                          '/data/theorie/jthoeve/ML4EFT_higgs/models/2022/24_01/model_lin_cHq3_3_feat/mc_run_{mc_run}']}
 
 sm_data_path = '/data/theorie/jthoeve/event_generation/events_high_stats/pT/sm/events_0.npy'
 
@@ -13,10 +16,14 @@ events_sm = np.load(sm_data_path)[1:, :]
 
 luminosity = 5000
 
+plot_save = '/data/theorie/jthoeve/ML4EFT_higgs/plots/2022/27_01/run_03/'
+
 # domain of EFT parameters to scan for limit setting
-cHW_values = np.linspace(-1, 1, 4)
-cHq3_values = np.linspace(-3, 3, 4)
-scan_domain = np.array([cHW_values, cHq3_values])
+cHW_values = np.linspace(-0.5, 0.2, 50)
+cHq3_values = np.linspace(-1.0, 2.0, 50)
+if int(row) == 0:
+    np.save(os.path.join(plot_save, 'scan_domain.npy'), np.array([cHW_values, cHq3_values]))
+scan_domain = (cHW_values, [cHq3_values[int(row)]])
 
 # number of bins to consider
 bins = np.array([2])
@@ -25,7 +32,7 @@ bins = np.array([2])
 mc_reps = 30
 
 # location where to save the plot
-plot_save = '/data/theorie/jthoeve/ML4EFT_higgs/plots/2022/24_01/'
+
 
 limits = conf_int.Limits(luminosity=luminosity,
                          bins=bins,
@@ -36,5 +43,9 @@ limits = conf_int.Limits(luminosity=luminosity,
                          lin=True,
                          plot_path=plot_save,
                          architecture=architecture,
+                         row=int(row),
                          save=True,
-                         save_path='/data/theorie/jthoeve/ML4EFT_higgs/plots/2022/24_01')
+                         save_path='/data/theorie/jthoeve/ML4EFT_higgs/plots/2022/27_01/run_03/',
+                         nn = False,
+                         binned=False,
+                         truth=True)
--- code/dev/uu_zh_2b2e.py
+++ code/dev/uu_zh_2b2e.py
@@ -5,7 +5,7 @@ from matplotlib import rc
 import lhapdf
 from matplotlib.ticker import FormatStrFormatter
 
-rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 14})
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 12})
 rc('text', usetex=True)
 
 p = lhapdf.mkPDF("NNPDF31_lo_as_0118", 0)
@@ -67,10 +67,13 @@ def get_d_eta(p1, p2):
     d_eta = np.abs(eta_2 - eta_1)
     return d_eta
 
-path_to_lhe_eft = '/Users/jaco/Documents/ML4EFT/MG5_aMC_v2.7.3/bin/uu_zh_eebb_cdhre_quad/Events/run_01_0/unweighted_events.lhe'
-path_to_lhe_sm = '/Users/jaco/Documents/ML4EFT/MG5_aMC_v2.7.3/bin/uu_zh_eedd_sm/Events/run_01_0/unweighted_events.lhe'
-#path_to_lhe = '/Users/jaco/Documents/ML4EFT/MG5_aMC_v2.7.3/bin/uu_zh_2b2e/Events/run_01/unweighted_events.lhe'
+# top flavour scheme
+path_to_lhe_eft = '/Users/jaco/Documents/ML4EFT/MG5_aMC_v2.7.3/bin/pp_zh_eebb_cHDD/Events/run_01_0/unweighted_events.lhe'
+path_to_lhe_sm = '/Users/jaco/Documents/ML4EFT/MG5_aMC_v2.7.3/bin/pp_zh_eebb_sm/Events/run_01_0/unweighted_events.lhe'
 
+# topU31 flavour scheme
+# path_to_lhe_eft = '/Users/jaco/Documents/ML4EFT/MG5_aMC_v2.7.3/bin/pp_zh_eebb_cHDD_topU3l/Events/run_01_0/unweighted_events.lhe'
+# path_to_lhe_sm = '/Users/jaco/Documents/ML4EFT/MG5_aMC_v2.7.3/bin/pp_zh_eebb_sm_topU3l/Events/run_01_0/unweighted_events.lhe'
 
 
 def read_events(path_to_lhe):
@@ -82,13 +85,15 @@ def read_events(path_to_lhe):
         if not found_weight:
             tot_xsec = e.eventinfo.weight
             found_weight = True
-            data_madgraph.append([tot_xsec, tot_xsec, tot_xsec, tot_xsec])
+            data_madgraph.append([tot_xsec, tot_xsec, tot_xsec, tot_xsec, tot_xsec, tot_xsec])
 
         d_eta_bb = np.abs(get_d_eta(e.particles[-1], e.particles[-2]))
         d_phi_bb = np.abs(get_d_phi(e.particles[-1], e.particles[-2]))
         d_R_bb = np.sqrt(d_eta_bb ** 2 + d_phi_bb ** 2)
         m_bb = get_inv_mass(e.particles[-1], e.particles[-2])
-        data_madgraph.append([d_eta_bb, d_phi_bb, d_R_bb, m_bb])
+        pt_b2 = get_pt(e.particles[-1])
+        pt_b1 = get_pt(e.particles[-2])
+        data_madgraph.append([d_eta_bb, d_phi_bb, d_R_bb, m_bb, pt_b1, pt_b2])
     data_madgraph = np.array(data_madgraph)
     return data_madgraph
 
@@ -98,34 +103,41 @@ data_eft = read_events(path_to_lhe_eft)
 bin_width = 0.1
 
 n_cols = 2
-n_rows = 2
-fig = plt.figure(figsize=(n_cols * 6, n_rows * 4))
+n_rows = 3
+fig = plt.figure(figsize=(n_cols * 5, n_rows * 4))
 
-x_labels = [r'$|\Delta\eta(b_1, b_2)|$', r'$\Delta\phi(b_1, b_2)$', r'$\Delta R(b_1, b_2)$', r'$m_{b\bar{b}}\;[\mathrm{GeV}]$']
-y_labels = [r'$d\sigma/d\Delta\eta\;\mathrm{[pb]}$', r'$d\sigma/d\Delta\phi\;\mathrm{[pb]}$', r'$d\sigma/d\Delta R\;\mathrm{[pb]}$', r'$d\sigma/dm_{b\bar{b}}\;\mathrm{[pb\:GeV^{-1}]}$']
+x_labels = [r'$|\Delta\eta(b_1, b_2)|$', r'$\Delta\phi(b_1, b_2)$', r'$\Delta R(b_1, b_2)$', r'$m_{b\bar{b}}\;[\mathrm{GeV}]$', r'$p_T^{b_1}\;[\mathrm{GeV}]$', r'$p_T^{b_2}\;[\mathrm{GeV}]$']
+y_labels = [r'$1/\sigma(d\sigma/d\Delta\eta)$', r'$1/\sigma(d\sigma/d\Delta\phi)$', r'$1/\sigma(d\sigma/d\Delta R)$', r'$1/\sigma(d\sigma/dm_{b\bar{b}})\;\mathrm{[GeV^{-1}]}$',  r'$1/\sigma(d\sigma/dp_T^{b_2})\;\mathrm{[GeV^{-1}]}$',  r'$1/\sigma(d\sigma/dp_T^{b_2})\;\mathrm{[GeV^{-1}]}$']
 
 for i in range(n_cols * n_rows):
     ax = plt.subplot(n_rows, n_cols, i + 1)
     events_sm = data_sm[1:, i]
     hist_mg_sm, bins_mg = np.histogram(events_sm,
-                                    bins=np.linspace(np.min(events_sm), np.max(events_sm), 40),
+                                    bins=np.linspace(np.min(events_sm), np.max(events_sm), 30),
                                     density=True)
     #hist_mg_sm *= data_sm[0, 0]
 
     events_eft = data_eft[1:, i]
     hist_mg_eft, _ = np.histogram(events_eft,
-                                       bins=np.linspace(np.min(events_eft), np.max(events_eft), 40),
+                                       bins=np.linspace(np.min(events_eft), np.max(events_eft), 30),
                                        density=True)
     #hist_mg_eft *= data_eft[0, 0]
 
     plt.step(bins_mg[:-1], hist_mg_sm, c='C0', where='post', label= r'$\mathrm{SM}$')
-    plt.step(bins_mg[:-1], hist_mg_eft, c='C1', where='post', label=r'$\mathrm{EFT}$')
-    plt.xlim((np.min(events_sm), np.max(events_eft)))
+    plt.step(bins_mg[:-1], hist_mg_eft, c='C1', where='post', label=r'$\mathrm{EFT}\;(\mathrm{cHD}=10)$')
+    if i > 3:
+        plt.xlim((np.min(events_sm), 500))
+    else:
+        plt.xlim((np.min(events_sm), np.max(events_eft)))
     plt.ylabel(y_labels[i])
     plt.xlabel(x_labels[i])
     plt.yscale('log')
     ax.xaxis.set_major_formatter(FormatStrFormatter(r'$%.2f$'))
-    plt.legend()
+    # ax.text(0.05, 0.05, r'$\mathrm{cHD}=10$',
+    #      horizontalalignment='left',
+    #      verticalalignment='center',
+    #      transform=ax.transAxes)
+    plt.legend(fontsize=10)
 
 #plt.show()
-plt.savefig('/Users/jaco/Documents/ML4EFT/plots/2022/31_01/decay_kinematics_bb.pdf')
+plt.savefig('/Users/jaco/Documents/ML4EFT/plots/2022/31_01/decay_kinematics_bb_6.pdf')
