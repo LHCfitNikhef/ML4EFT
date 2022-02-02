e076352feafd418432bd473b338386c75ec004b7
jacoterh <54140851+jacoterh@users.noreply.github.com>
2022-02-02 21:19:24 +0100

    adding lhapdf option to mg5 submit script
    Former-commit-id: 8a9264ba966b497e1d03724c00088878a1b9ef5b [formerly 5d56bba12b53516169ab55cd7503bda980b9372b]
    Former-commit-id: 04fac4046868d69b5f263871e43035200b98bd22

--- code/cluster/launch_scripts/event_generation/MGbatch_madevent.sh
+++ code/cluster/launch_scripts/event_generation/MGbatch_madevent.sh
@@ -6,12 +6,12 @@ PY=/data/theorie/jthoeve/miniconda3/envs/ml4eft/bin/python
 Base=$HOMEDIR/MGjobs
 mkdir -p $Base
 
-for (( i=1; i<=2; i++ ));
+chdd=(0 10)
+for (( i=1; i<=50; i++ ));
 do
 
-jobBase="$Base/$1/job$i"
 # $1 is given as input by the usr: the output name dir
-#cardBase=$jobBase/MG5_aMC_v3_3_1/$1/Cards
+jobBase="$Base/$1/job$i"
 
 mkdir -p $jobBase
 cp -r $Base/$1/$1/. $jobBase
@@ -26,14 +26,14 @@ echo "$PY $jobBase/bin/madevent $jobBase/cmd" >> MGscript.sh
 # create the run file
 echo "launch" >> cmd
 echo "0" >> cmd
-echo "set chdd 10" >> cmd
-echo "set nevents 1000" >> cmd
+echo "set pdlabel lhapdf" >> cmd
+echo "set lhaid 315000" >> cmd
+echo "set chdd ${chdd[i-1]}" >> cmd
+echo "set nevents 100000" >> cmd
 echo "set ebeam1 7000" >> cmd
 echo "set ebeam2 7000" >> cmd
 echo "set use_syst False" >> cmd
-echo "set iseed $i"
-
-#sed -i "33s/.*/      $i       = iseed   ! rnd seed (0=assigned automatically=default))/" $jobBase/MG5_aMC_v3_3_1/bin/test_run0/Cards/run_card.dat
+echo "set iseed $i" >> cmd
 
 chmod +x MGscript.sh
 qsub -q smefit -W group_list=smefit MGscript.sh
