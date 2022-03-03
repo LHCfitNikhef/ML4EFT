732767b737d3b39417cacc0fee58830eee85ec6e
jacoterh <54140851+jacoterh@users.noreply.github.com>
2022-03-03 10:55:05 +0100

    adding truth contour with 3 features
    zh (no decays)
    
    
    Former-commit-id: d4ba68596f7ac3f01f2fb2a276ff4968419a0ab4 [formerly 174808a473ddb40ce75a2ff43e6e87dbce282e26]
    Former-commit-id: 325848b70d20b7f0128417825e3967f8d69508ff

---
+++ code/dev/test.py
@@ -0,0 +1,139 @@
+# %%
+from quad_clas.core.th_predictions import TheoryPred
+from quad_clas.plotting.plot_contours import plot_contours, plot_contours_binned
+import numpy as np
+
+# %%
+
+coeffs = ['chdd', 'chwb', 'cbhre', 'chw']
+bins = np.array([0, 75, 150, 250, 400, 1000])
+
+theory_pred = TheoryPred(coeffs, bins, '/Users/jaco/Documents/ML4EFT/training_data/zh_llbb', nreps=50)
+
+xsec_df = theory_pred.df.sum(axis=1)
+
+c_x, c_y = np.load('/Users/jaco/Documents/ML4EFT/23_02/scan_domain.npy')
+path_to_row = '/Users/jaco/Documents/ML4EFT/23_02/q_c_nn_median_row_{}.npy'
+label_legend = r'$\rm{NN\;8\;features}$'
+label_x = r'$\rm{cHW}$'
+label_y = r'$\rm{cHWB}$'
+
+path_to_row = '/Users/jaco/Documents/ML4EFT/output/contours/nn/q_c_nn_median_row_{}.npy'
+# fig = plot_contours(c_x, c_y, path_to_row, label_legend, label_x, label_y, xsec_df, binned=True, path_to_row_binned='/Users/jaco/Documents/ML4EFT/output/contours/binned/q_c_binned_row_{}.npy')
+
+fig = plot_contours(c_x, c_y, path_to_row, label_legend, label_x, label_y, xsec_df)
+fig.savefig('/Users/jaco/Documents/ML4EFT/code/dev/contour_nn_debug.pdf')
+
+# %%
+import os
+import re
+
+filename = '/Users/jaco/Documents/ML4EFT/training_data/zh/features_mzh_y_ptz/lin/cHq3/param_card.dat'
+#match_number = re.compile('[-+]? *[0-9]+.?[0-9]*(?:[Ee] *[-+]? *[0-9]+)?')
+match_number = re.compile('[-+]?\ *[0-9]+\.?[0-9]*(?:[Ee]\ *[-+]?\ *[0-9]+)?')
+
+with open(filename, 'r') as f:
+    data = f.readlines()
+    f.close()
+# %%
+pattern = re.compile(r'# cHq3\s+', re.IGNORECASE)
+for i, line in enumerate(data):
+    if len(pattern.findall(line)) > 0:
+        print([x for x in pattern.findall(line)])
+        matched_line = [x for x in match_number.findall(line)]
+        break
+param = float(matched_line[-1])
+# final_list = [x for x in re.findall(pattern, line)]
+#
+# matches = pattern.finditer(line)
+# for match in matches:
+#     print(i, match)
+
+#%%
+coeffs = ['cHq3', 'cHW']
+bins = np.array([0, 75, 150, 250, 400, 1000]) * 1E-3
+theory_pred = TheoryPred(coeffs, bins, '/Users/jaco/Documents/ML4EFT/training_data/zh/features_mzh_y_ptz', nreps=30)
+
+
+#%%
+# zh (no decays, 2 features)
+
+coeffs = ['cHq3', 'cHW']
+bins = np.array([0, 75, 150, 250, 400, 1000])
+
+# cHW_values = np.linspace(-1, 0.5, 100)
+# cHq3_values = np.linspace(-2, 3, 100)
+
+#theory_pred = TheoryPred(coeffs, bins, '/Users/jaco/Documents/ML4EFT/training_data/zh_llbb', nreps=50)
+
+#xsec_df = theory_pred.df.sum(axis=1)
+
+c_x, c_y = np.load('/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_1/scan_domain.npy') #chw, chq3
+#path_to_row = '/Users/jaco/Documents/ML4EFT/23_02/q_c_nn_median_row_{}.npy'
+label_legend = r'$\rm{binned}$'
+label_x = r'$\rm{cHW}$'
+label_y = r'$\rm{cHq3}$'
+path_to_row_truth='/Users/jaco/Documents/ML4EFT/output/contours/zh/truth/q_c_truth_row_{}.npy'
+path_to_row_nn='/Users/jaco/Documents/ML4EFT/output/contours/zh/nn/q_c_nn_median_row_{}.npy'
+
+
+#path_to_row = '/Users/jaco/Documents/ML4EFT/output/contours/nn/q_c_nn_median_row_{}.npy'
+xsec_df = None
+fig = plot_contours(c_x, c_y, path_to_row_truth, path_to_row_nn, label_legend, label_x, label_y, xsec_df, binned=True, path_to_row_binned='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_1/q_c_binned_row_{}.npy',
+                    path_to_row_binned_2='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_2/q_c_binned_row_{}.npy',
+                    path_to_row_binned_3='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_3/q_c_binned_row_{}.npy',
+                    path_to_row_binned_4='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_4/q_c_binned_row_{}.npy')
+fig.savefig('/Users/jaco/Documents/ML4EFT/output/contours/zh/truth/truth_v4.pdf')
+
+#%%
+# zh (no decays, 3 features)
+
+coeffs = ['cHq3', 'chw']
+bins = np.array([0, 75, 150, 250, 400, 1000])
+
+# cHW_values = np.linspace(-1, 0.5, 100)
+# cHq3_values = np.linspace(-2, 3, 100)
+
+#theory_pred = TheoryPred(coeffs, bins, '/Users/jaco/Documents/ML4EFT/training_data/zh_llbb', nreps=50)
+
+#xsec_df = theory_pred.df.sum(axis=1)
+
+c_x, c_y = np.load('/Users/jaco/Documents/ML4EFT/output/contours/zh/features_mzh_y_ptz/truth/scan_domain.npy') #chw, chq3
+#path_to_row = '/Users/jaco/Documents/ML4EFT/23_02/q_c_nn_median_row_{}.npy'
+label_legend = r'$\rm{binned}$'
+label_x = r'$\rm{cHW}$'
+label_y = r'$\rm{cHq3}$'
+path_to_row_truth='/Users/jaco/Documents/ML4EFT/output/contours/zh/features_mzh_y_ptz/truth/q_c_truth_row_{}.npy'
+path_to_row_truth_2='/Users/jaco/Documents/ML4EFT/output/contours/zh/features_mzh_y/truth/q_c_truth_row_{}.npy'
+path_to_row_nn=None
+
+theory_pred = TheoryPred(coeffs, bins, '/Users/jaco/Documents/ML4EFT/training_data/zh/features_mzh_y_ptz', nreps=30)
+xsec_df = theory_pred.df.sum(axis=1)
+
+
+#path_to_row = '/Users/jaco/Documents/ML4EFT/output/contours/nn/q_c_nn_median_row_{}.npy'
+
+fig = plot_contours(c_x, c_y, path_to_row_truth, path_to_row_truth_2, path_to_row_nn, label_legend, label_x, label_y, xsec_df, binned=False, path_to_row_binned='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_1/q_c_binned_row_{}.npy',
+                    path_to_row_binned_2='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_2/q_c_binned_row_{}.npy',
+                    path_to_row_binned_3='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_3/q_c_binned_row_{}.npy',
+                    path_to_row_binned_4='/Users/jaco/Documents/ML4EFT/output/contours/zh/binned/binning_4/q_c_binned_row_{}.npy')
+fig.savefig('/Users/jaco/Documents/ML4EFT/output/contours/zh/features_mzh_y_ptz/truth/truth.pdf')
+#%%
+
+c_x, c_y = np.load('/Users/jaco/Documents/ML4EFT/output/contours/zh_llbb/binned/binning_1/scan_domain.npy') #chw, chq3
+
+label_legend = r'$\rm{binned}$'
+label_x = r'$\rm{cHW}$'
+label_y = r'$\rm{cHWB}$'
+
+path_to_row_truth=None
+path_to_row_nn='/Users/jaco/Documents/ML4EFT/output/contours/zh_llbb/nn/q_c_nn_median_row_{}.npy'
+bins = np.array([0, 75, 150, 250, 400, 1000])
+coeffs = ['chw', 'chwb']
+theory_pred = TheoryPred(coeffs, bins, '/Users/jaco/Documents/ML4EFT/training_data/zh_llbb', nreps=50)
+xsec_df = theory_pred.df.sum(axis=1)
+
+fig = plot_contours(c_x, c_y, path_to_row_truth, path_to_row_nn, label_legend, label_x, label_y, xsec_df, binned=True,
+                    path_to_row_binned='/Users/jaco/Documents/ML4EFT/output/contours/zh_llbb/binned/binning_1/q_c_binned_row_{}.npy',
+                    path_to_row_binned_2='/Users/jaco/Documents/ML4EFT/output/contours/zh_llbb/binned/binning_2/q_c_binned_row_{}.npy')
+fig.savefig('/Users/jaco/Documents/ML4EFT/output/contours/zh_llbb/nn/nn.pdf')
--- code/src/quad_clas/plotting/plot_contours.py
+++ code/src/quad_clas/plotting/plot_contours.py
@@ -4,6 +4,9 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import seaborn
+import copy
+import matplotlib as mpl
+from matplotlib import cm
 
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 22})
 rc('text', usetex=True)
@@ -25,100 +28,124 @@ def combine_likelihood_binned(c_x, c_y, path):
         likelihood[row, :] = values
     return likelihood
 
-def plot_contours(c_x, c_y, path_to_row_truth, path_to_row_nn, label_legend, label_x, label_y, xsec_df, binned=False, path_to_row_binned=None, path_to_row_binned_2=None,
+def plot_contours(c_x, c_y, path_to_row_truth, path_to_row_truth_2, path_to_row_nn, label_legend, label_x, label_y, xsec_df, binned=False, path_to_row_binned=None, path_to_row_binned_2=None,
                   path_to_row_binned_3=None, path_to_row_binned_4=None):
 
     yy, xx = np.meshgrid(c_y, c_x, indexing='ij')  # c_y is y-axis
     fig, ax = plt.subplots(figsize=(12, 10))
 
-    q_c_truth = combine_qc(c_x, c_y, path_to_row_truth)
-    q_c_nn = combine_qc(c_x, c_y, path_to_row_nn)
-
-    # from scipy.ndimage.filters import gaussian_filter
-    # q_c_truth = gaussian_filter(q_c_truth, sigma=.6)
-
-    # contour_truth = plt.contourf(xx, yy, q_c_truth, np.array([0, 5.99]), origin='lower',
-    #                          colors='C0', alpha=0.2)
-    contour_truth = plt.contour(xx, yy, q_c_truth, np.array([5.99]), origin='lower', linestyles='dashed',
-                                 linewidths=1.5,
-                                 colors='k')
-
-    # contour_nn = plt.contourf(xx, yy, q_c_nn, np.array([0, 5.99]), origin='lower',
-    #                           colors='C1', alpha=0.2)
-
-    contour_nn = plt.contour(xx, yy, q_c_nn, np.array([5.99]), origin='lower', linestyles='dashed',
-                              linewidths=1.5,
-                              colors='red')
-
-    #seaborn.heatmap(q_c_nn)
-    from matplotlib import cm
-    #norm = cm.colors.Normalize(vmax=abs(q_c_nn).max(), vmin=-abs(q_c_nn).max())
-    #cmap = cm.PRGn
-    #im = ax.imshow(q_c_nn, origin='lower', extent =(-0.5, -0.10, -1.0, 1.5), cmap=cmap, norm=norm, aspect='auto')
-    #fig.colorbar(im, ax=ax)
-
-
     labels = []
     contours = []
 
-    contour_handle, _ = contour_truth.legend_elements()
-    contours.append(contour_handle[0])
+    if path_to_row_truth is not None:
+        q_c_truth = combine_qc(c_x, c_y, path_to_row_truth)
 
-    labels.append(r'$\rm{Truth}$')
+        # contour_truth = plt.contourf(xx, yy, q_c_truth, np.array([0, 5.99]), origin='lower',
+        #                          colors='C0', alpha=0.2)
+        contour_truth = plt.contour(xx, yy, q_c_truth, np.array([5.99]), origin='lower', linestyles='dashed',
+                                    linewidths=1.5,
+                                    colors='k')
 
-    contour_handle, _ = contour_nn.legend_elements()
-    contours.append(contour_handle[0])
+        contour_handle, _ = contour_truth.legend_elements()
+        contours.append(contour_handle[0])
 
-    labels.append(r'$\rm{NN}$')
+        labels.append(r'$\rm{Truth}\;(3\;\rm{features})$')
+
+        q_c_truth_2 = combine_qc(c_x, c_y, path_to_row_truth_2)
+
+        # contour_truth = plt.contourf(xx, yy, q_c_truth, np.array([0, 5.99]), origin='lower',
+        #                          colors='C0', alpha=0.2)
+        contour_truth = plt.contour(xx, yy, q_c_truth_2, np.array([5.99]), origin='lower', linestyles='dashed',
+                                    linewidths=1.5,
+                                    colors='blue')
+
+        contour_handle, _ = contour_truth.legend_elements()
+        contours.append(contour_handle[0])
+
+        labels.append(r'$\rm{Truth}\;(2\;\rm{features})$')
+
+
+    rc = -xsec_df['chw'] / xsec_df['cHq3']
+    if path_to_row_nn is not None:
+        q_c_nn = combine_qc(c_x, c_y, path_to_row_nn)
+
+        q_c_nn = np.ma.masked_where(yy < rc * xx - 0.05, q_c_nn)
+
+        contour_nn = plt.contour(xx, yy, q_c_nn,np.array([np.nanmin(q_c_nn) + 5.99]), origin='lower', linestyles='dashed',
+                                 linewidths=1.5,
+                                 colors='red')
+
+        contour_handle, _ = contour_nn.legend_elements()
+        contours.append(contour_handle[0])
+
+        labels.append(r'$\rm{NN}$')
 
     plt.title(r'$95\%\rm{\;CL\;intervals}$')
     plt.xlabel(label_x)
     plt.ylabel(label_y)
     plt.scatter(0, 0, marker='+', label='SM', color='k')
+
+
+    # norm = mpl.colors.Normalize(vmin=np.nanmin(q_c_nn), vmax= np.nanmax(q_c_nn))#mpl.colors.BoundaryNorm(bounds, cmap_copy.N, extend='both')
+    # cmap = 'GnBu'
+    # cmap_copy = copy.copy(mpl.cm.get_cmap(cmap))
+    # cmap_copy.set_bad(color='gainsboro')
+    #
+    # xsec = xsec_df['sm'] * np.ones(xx.shape) + xsec_df['chw'] * xx + xsec_df['chwb'] * yy
+    # xsec_masked = np.ma.masked_where(xsec < 0, xsec)
+    #
+    # #im = ax.imshow(xsec_masked, origin='lower', extent=(c_x.min(), c_x.max(), c_y.min(), c_y.max()))
+    # im = ax.imshow(q_c_nn, origin='lower', extent=(c_x.min(), c_x.max(), c_y.min(), c_y.max()), cmap=cmap_copy)
+    # cbar = plt.colorbar(mpl.cm.ScalarMappable(norm = norm, cmap=cmap_copy), ax=ax)
+
     #idx_q_c_min = np.argwhere(q_c_nn == 0)
     #plt.scatter(c_x[idx_q_c_min[0, 1]], c_y[idx_q_c_min[0, 0]], marker='x', color='k')
 
-    #rc = -xsec_df['chwb'] / xsec_df['chw']
-    #plt.plot(c_x, c_y * rc, linestyle='dotted', label=r'$\rm{flat}\;\rm{direction}$')
-    # cHW_values = np.linspace(-1, 0.5, 100)
-    # cHq3_values = np.linspace(-2, 3, 100)
+
+    plt.plot(c_x, c_x * rc, linestyle='dotted', label=r'$\rm{flat}\;\rm{direction}$')
 
     if binned:
+
         log_likelihood_2 = combine_likelihood_binned(c_x, c_y, path_to_row_binned_2)
         contour_binned = ax.contour(log_likelihood_2, np.array([np.max(log_likelihood_2) - 0.5 * 5.99]),
                                     origin='lower', linestyles='dashed', linewidths=1.0, colors='C0',
-                                    extent=(-1, 0.5, -2, 3))
+                                    extent=(c_x.min(), c_x.max(), c_y.min(), c_y.max()))
         contour_handle, _ = contour_binned.legend_elements()
         contours.append(contour_handle[0])
         labels.append(r'$1\;\rm{bin}$')
 
-        log_likelihood_4 = combine_likelihood_binned(c_x, c_y, path_to_row_binned_4)
-        contour_binned = ax.contour(log_likelihood_4, np.array([np.max(log_likelihood_4) - 0.5 * 5.99]),
-                                    origin='lower', linestyles='dashed', linewidths=1.0, colors='C1',
-                                    extent=(-1, 0.5, -2, 3))
-        contour_handle, _ = contour_binned.legend_elements()
-        contours.append(contour_handle[0])
-        labels.append(r'$2\;\rm{bins}$')
-
-        log_likelihood_3 = combine_likelihood_binned(c_x, c_y, path_to_row_binned_3)
-        contour_binned = ax.contour(log_likelihood_3, np.array([np.max(log_likelihood_3) - 0.5 * 5.99]),
-                                    origin='lower', linestyles='dashed', linewidths=1.0, colors='C2',
-                                    extent=(-1, 0.5, -2, 3))
-        contour_handle, _ = contour_binned.legend_elements()
-        contours.append(contour_handle[0])
-        labels.append(r'$4\;\rm{bins}$')
-
+        # log_likelihood_4 = combine_likelihood_binned(c_x, c_y, path_to_row_binned_4)
+        # contour_binned = ax.contour(log_likelihood_4, np.array([np.max(log_likelihood_4) - 0.5 * 5.99]),
+        #                             origin='lower', linestyles='dashed', linewidths=1.0, colors='C1',
+        #                             extent=(-1, 0.5, -2, 3))
+        # contour_handle, _ = contour_binned.legend_elements()
+        # contours.append(contour_handle[0])
+        # labels.append(r'$2\;\rm{bins}$')
+        #
+        # log_likelihood_3 = combine_likelihood_binned(c_x, c_y, path_to_row_binned_3)
+        # contour_binned = ax.contour(log_likelihood_3, np.array([np.max(log_likelihood_3) - 0.5 * 5.99]),
+        #                             origin='lower', linestyles='dashed', linewidths=1.0, colors='C2',
+        #                             extent=(-1, 0.5, -2, 3))
+        # contour_handle, _ = contour_binned.legend_elements()
+        # contours.append(contour_handle[0])
+        # labels.append(r'$4\;\rm{bins}$')
+        #
         log_likelihood = combine_likelihood_binned(c_x, c_y, path_to_row_binned)
+        # extent = (x_min, x_max, y_min, y_max) = (cHW, cHq3)
         contour_binned = ax.contour(log_likelihood, np.array([np.max(log_likelihood) - 0.5 * 5.99]),
-                              origin='lower', linestyles='dashed', linewidths=1.0, colors='C4', extent=(-1, 0.5, -2, 3))
+                              origin='lower', linestyles='dashed', linewidths=1.0, colors='C4', extent=(c_x.min(), c_x.max(), c_y.min(), c_y.max()))
         contour_handle, _ = contour_binned.legend_elements()
         contours.append(contour_handle[0])
-        labels.append(r'$6\;\rm{bins}$')
+        labels.append(r'$5\;\rm{bins}$')
+
+
 
 
     plt.legend(contours, labels, fontsize=15, frameon=False, loc='best')
+    # plt.xlim(c_x.min(), c_x.max())
+    # plt.ylim(c_y.min(), c_y.max())
     plt.xlim(-0.5, 0.3)
-    plt.ylim(-1.5, 2.3)
+    plt.ylim(-1,2)
     return fig
 
 def plot_contours_binned(c_x, c_y, path_to_row):
