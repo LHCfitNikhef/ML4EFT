99373027ea9fbbaecc86827906e9b855dd490e8f
jacoterh <54140851+jacoterh@users.noreply.github.com>
2022-03-08 14:05:02 +0100

    adding smefit benchmark theory prediction extraction
    Former-commit-id: 88dca3a8b77894f70da2e6c529f2c4c354de931d [formerly bdf2f9b0701d695b1630fa7423b1b37667fe8c23]
    Former-commit-id: 48fdb658cf8fd9fc44e05f85e00499779c5bc8c9

---
+++ code/dev/smefit_benchmark.py
@@ -0,0 +1,21 @@
+#%%
+from quad_clas.core.th_predictions import TheoryPred
+import numpy as np
+import pandas as pd
+#%% experimental data
+lumi = 5E6
+bins = np.array([0, 75, 150, 250, 400, 1000])
+observed_data = pd.read_pickle('/Users/jaco/Documents/ML4EFT/output/fitmaker_vs_ml4eft/observed_data.pkl.gz')
+n_i, _ = np.histogram(observed_data['pt_z'].values, bins)
+#%%
+sigma_i = n_i / lumi
+stat_unc_sigma_i = np.sqrt(n_i) / lumi
+
+#%% theory predictions
+coeffs = ['chw', 'chwb']
+
+theory_pred = TheoryPred(coeff=coeffs,
+                         bins=bins,
+                         kinematic='pt_z',
+                         event_path='/Users/jaco/Documents/ML4EFT/training_data/zh_llbb/',
+                         nreps=50)
