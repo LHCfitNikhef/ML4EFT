9191adb28446d46c5037084bfa62da756c923e87
jacoterh <54140851+jacoterh@users.noreply.github.com>
2022-04-03 14:57:20 +0200

    adding binned ns option
    Former-commit-id: a164149065119d9fc5276118087c7001d08fec2f [formerly 8e899f0c00025b4c16bdfe277d941ba032a25d61]
    Former-commit-id: 2a798bd874e22ca5da3b44c408abea8632b8ce78

--- code/src/quad_clas/limits/optimize_ns.py
+++ code/src/quad_clas/limits/optimize_ns.py
@@ -4,10 +4,12 @@ import sys
 import numpy as np
 import time
 import os
+import pandas as pd
 
 import quad_clas.analyse.analyse as analyse
 from quad_clas.core.truth import vh_prod
 from quad_clas.preproc import constants
+from quad_clas.core.th_predictions import TheoryPred
 
 mz = constants.mz
 mh = constants.mh
@@ -26,9 +28,17 @@ class Optimize:
             instance of TheoryPred that contains the theory predictions
     """
 
-    def __init__(self, config, observed_data, theory_pred):
+    def __init__(self, config):
 
         self.config = config.copy()
+
+        if "mode" in self.config.keys():
+            self.mode = self.config["mode"]
+        else:
+            print(
+                "No mode (mode) selected, please specify one in the input card. Aborting."
+            )
+            sys.exit()
 
         if "parameters" in self.config.keys():
             self.parameters = self.config["parameters"]
@@ -59,12 +69,53 @@ class Optimize:
                 "Luminosity (lumi) not set in the input card. Using default: 5e3 pb^-1"
             )
 
-        self.observed_data = observed_data
+        if self.mode == 'binned':
+            if "bins" in self.config.keys():
+                self.bins = self.config["bins"]
+            else:
+                print(
+                    "No binning (bins) specified. Please set in the in the input card. Aborting."
+                )
+                sys.exit()
+            if "kinematic" in self.config.keys():
+                self.kinematic = self.config["kinematic"]
+            else:
+                print(
+                    "No bin variable (kinematic) specified. Please set in the in the input card. Aborting."
+                )
+                sys.exit()
+        else:  # unbinned case
+            self.bins = None,
+            self.kinematic = None
 
-        # likelihood building blocks
-        self.dsigma_dx_sm, self.dsigma_dx_eft = theory_pred.compute_diff_coefficients(self.observed_data)
-        self.theory_pred_total = theory_pred.df.sum(axis=1)
+        event_path = self.config['theory_pred_path']
+        theory_pred = TheoryPred(coeff=self.parameters,
+                                 event_path=event_path,
+                                 nreps=30,
+                                 bins=self.bins,
+                                 kinematic=self.kinematic)
 
+        # load dataset (pseudo data)
+        sm_data_path = self.config['observed_data_path']
+        sm_data = pd.read_pickle(sm_data_path).iloc[1:, :]
+
+        # construct observed dataset
+
+        theory_pred_total = theory_pred.df.sum(axis=1)
+        xsec_sm = theory_pred_total['sm']
+        nu_tot_sm = xsec_sm * config['lumi']
+        n_tot_sm = np.random.poisson(nu_tot_sm, 1)
+
+        self.observed_data = sm_data.sample(int(n_tot_sm), random_state=1)
+
+        # observed counts
+        if self.mode == "unbinned":
+            self.dsigma_dx_sm, self.dsigma_dx_eft = theory_pred.compute_diff_coefficients(self.observed_data)
+        elif self.mode == "binned":
+            self.n_i, _ = np.histogram(self.observed_data[self.kinematic].values, self.bins)
+
+        # theory predictions
+        self.theory_pred = theory_pred.df
 
     def my_prior(self, cube):
         """
@@ -85,19 +136,29 @@ class Optimize:
 
         return cube
 
-    def my_log_like(self, cube):
+    def log_like_truth(self, cube):
 
-        sigma = self.theory_pred_total['sm'] + cube[0] * self.theory_pred_total['chw'] + cube[1] * self.theory_pred_total['chq3']
+        theory_pred_total = self.theory_pred.sum(axis=1)
+
+        sigma = theory_pred_total['sm'] + cube[0] * theory_pred_total['chw'] + cube[1] * theory_pred_total['chq3']
 
         dsigma_dx = self.dsigma_dx_sm + self.dsigma_dx_eft['lin'].T @ cube
-
-        #dsigma_dx = self.dsigma_dx_sm + cube[0] * (self.dsigma_dx_c1 - self.dsigma_dx_sm) / 10 + cube[1] * (self.dsigma_dx_c2 - self.dsigma_dx_sm) / 10
 
         nu = sigma * self.lumi
 
         log_likelihood = -nu + np.sum(np.log(dsigma_dx))
 
         return log_likelihood
+
+    def log_like_binned(self, cube):
+        
+        sigma_i = self.theory_pred.loc['sm'] + self.theory_pred.iloc[1:, :].T @ cube
+        nu_i = sigma_i * self.lumi
+        log_l_i = self.n_i * np.log(nu_i) - nu_i
+
+        #log_l_i[np.isnan(log_l_i)] = 0
+        return log_l_i.sum()
+
 
     def run_sampling(self):
         """Run the minimisation with Nested Sampling"""
@@ -107,16 +168,21 @@ class Optimize:
 
         t1 = time.perf_counter()
 
-        result = pymultinest.solve(LogLikelihood=self.my_log_like,
-                                 Prior=self.my_prior,
-                                 n_dims=self.n_params,
-                                 outputfiles_basename=prefix,
-                                 verbose=True,
-                                 n_live_points=self.live_points,
-                                 const_efficiency_mode=False,
-                                 resume=False,
-                                 importance_nested_sampling=True
-                                 )
+        if self.mode == "unbinned":
+            log_likelihood = self.log_like_truth
+        elif self.mode == "binned":
+            log_likelihood = self.log_like_binned
+
+        result = pymultinest.solve(LogLikelihood=log_likelihood,
+                                   Prior=self.my_prior,
+                                   n_dims=self.n_params,
+                                   outputfiles_basename=prefix,
+                                   verbose=True,
+                                   n_live_points=self.live_points,
+                                   const_efficiency_mode=False,
+                                   resume=False,
+                                   importance_nested_sampling=True
+                                   )
 
         t2 = time.perf_counter()
 
