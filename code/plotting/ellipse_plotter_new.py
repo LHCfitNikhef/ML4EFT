0f17274efac71b4ab44f1d2ad4fd794de19f31af
Jaco ter Hoeve <jthoeve@stbc-i2.nikhef.nl>
2022-08-18 13:03:32 +0200

    fixing ellipse plotter
    Former-commit-id: 9d79396afca70f66e2e6faf927d69349670cd847 [formerly 8fe93a73f625055d893aead572a7062f80d54b1a]
    Former-commit-id: 5294377ba08439382146d690d62c8fc77a1a08d9

--- code/plotting/ellipse_plotter_new.py
+++ code/plotting/ellipse_plotter_new.py
@@ -13,6 +13,7 @@ import os
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 26})
 rc('text', usetex=True)
 
+
 class EllipsePlotter:
     """
     Class to plot 2 dimensional confidence levels bounds
@@ -26,7 +27,7 @@ class EllipsePlotter:
         pass
 
     @staticmethod
-    def confidence_ellipse(x, y, ax, n_std=3.0, facecolor="none", **kwargs):
+    def confidence_ellipse_smefit(x, y, ax, n_std=3.0, facecolor="none", **kwargs):
         """
         Create a plot of the covariance confidence ellipse of `x` and `y`
         Parameters
@@ -81,36 +82,70 @@ class EllipsePlotter:
         ellipse.set_transform(transf + ax.transData)
         return ax.add_patch(ellipse)
 
-    def get_coeffiecients_values(self, coeff, fit):
+    @staticmethod
+    def confidence_ellipse(x, y, ax, kde=False, facecolor="none", **kwargs):
         """
-        Get the coefficient posterior or look for
-        propagated params.
+        Create a plot of the covariance confidence ellipse of `x` and `y`
         Parameters
         ----------
-            coeff: str
-               coefficient name
-            fit: str
-                fit name
+        x, y : array_like, shape (n, )
+            Input data.
+        ax : matplotlib.axes.Axes
+            The axes object to draw the ellipse into.
+        n_std : float
+            The number of standard deviations to determine the ellipse's radiuses.
         Returns
         -------
-            coeff_values: numplt.ndarray
-                posterior distribution
+        matplotlib.patches.Ellipse
+        Other parameters
+        ----------------
+        kwargs : `~matplotlib.patches.Patch` properties
         """
-        # TODO: maybe you can look for something in coefficints
-        # utils, to avoid repetition
 
-        if coeff in self.full_labels[fit]:
-            # look for posteriors else for propagated params
-            if "SNS" in fit:
-                coeff_values = np.array(self.coeff_vals[fit][coeff])
-            else:
-                idx = np.where(np.array(self.full_labels[fit]) == coeff)[0][0]
-                coeff_values = np.array(self.coeff_vals[fit][coeff]).T[idx]
-        else:
-            coeff_values = None
-        return coeff_values
 
-    def plot(self, ax, dfs, ax_labels, coeff1, coeff2, samples=None):
+        if x.size != y.size:
+            raise ValueError("x and y must be the same size")
+
+        cov = np.cov(x, y)
+
+        eig_val, eig_vec = np.linalg.eig(cov)
+
+        eig_vec_max = eig_vec[:, np.argmax(eig_val)]
+
+        cos_th = eig_vec[np.argmax(eig_val), 0] / np.linalg.norm(eig_vec_max)
+        inclination = np.arccos(cos_th)
+
+        eigval_sort = np.sort(eig_val)
+
+        ell_radius_x = np.sqrt(2.3 * eigval_sort[-1])
+        ell_radius_y = np.sqrt(2.3 * eigval_sort[-2])
+
+        ellipse = Ellipse(
+            (0, 0),
+            width=ell_radius_x * 2,
+            height=ell_radius_y * 2,
+            facecolor=facecolor,
+            **kwargs
+        )
+
+        if kde:
+            sns.kdeplot(x, y, levels=[0.32, 1.0], bw_adjust=1.2, ax=ax, fill=True, alpha=0.3, color=facecolor)
+            sns.kdeplot(x, y, levels=[0.32], bw_adjust=1.2, ax=ax, alpha=1, color=facecolor)
+            ax.scatter(x, y, s=4, alpha=0.2)
+
+
+        mean_x = np.mean(x)
+        mean_y = np.mean(y)
+
+        transf = (transforms.Affine2D().rotate(inclination).translate(mean_x, mean_y))
+
+        ellipse.set_transform(transf + ax.transData)
+
+        ax.add_patch(ellipse)
+
+        return ax.add_patch(ellipse)
+
+    def plot(self, ax, dfs, ax_labels, coeff1, coeff2, kde=None, labels=None):
         """
         Plot the 2 standard deviation ellipse of two parameters
         (used for 2 parameter fits)
@@ -122,11 +157,7 @@ class EllipsePlotter:
                 second coefficient
         """
 
-        lbls = []
         hndls = []
-        p1 = []
-        p2 = []
-
         colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
         for i, df in enumerate(dfs):
@@ -135,74 +166,67 @@ class EllipsePlotter:
             coeff2_values = df[coeff2].values
 
             if coeff1_values is not None and coeff2_values is not None:
-                if samples is not None:
-                    if samples[i]:
-                        sns.kdeplot(x=coeff1_values, y=coeff2_values, levels=[0.32, 1.0], bw_adjust=1.2, ax=ax, fill=True, alpha=0.3, color=colors[i])
-                        sns.kdeplot(x=coeff1_values, y=coeff2_values, levels=[0.32], bw_adjust=1.2, ax=ax,
-                                    alpha=1, color=colors[i])
-                        hndls.append(mpatches.Patch(ec=colors[i], fc=colors[i], fill=True, alpha=0.3))
 
-                        ax.scatter(coeff1_values, coeff2_values, s=2, alpha=0.2, color=colors[i])
-
-                else:
-
-                    p2 = self.confidence_ellipse(
-                        coeff1_values,
-                        coeff2_values,
-                        ax,
-                        n_std=2,
-                        alpha=0.3,
-                        facecolor=colors[i],
-                        edgecolor=None,
-                    )
-                    p1 = self.confidence_ellipse(
-                        coeff1_values,
-                        coeff2_values,
-                        ax,
-                        n_std=2,
-                        alpha=1,
-                        edgecolor=colors[i],
-                    )
-
-
-                    p3 = self.confidence_ellipse(
-                        coeff1_values,
-                        coeff2_values,
-                        ax,
-                        n_std=1,
-                        alpha=1,
-                        edgecolor=colors[i],
-                        linestyle='dotted'
-                    )
-
-
-
-                    hndls.append((p1, p2))
-
-                ax.scatter(
-                    np.mean(coeff1_values, axis=0),
-                    np.mean(coeff2_values, axis=0),
-                    c=colors[i],
-                    s=3,
+                p1 = self.confidence_ellipse(
+                    coeff1_values,
+                    coeff2_values,
+                    ax,
+                    alpha=1,
+                    edgecolor=colors[i]
                 )
 
+                p2 = self.confidence_ellipse(
+                    coeff1_values,
+                    coeff2_values,
+                    ax,
+                    alpha=0.3,
+                    facecolor=colors[i],
+                    edgecolor=None,
+                    kde=kde[i]
+                )
+
+                # smefit
+
+                # p3 = self.confidence_ellipse_smefit(
+                #     coeff1_values,
+                #     coeff2_values,
+                #     ax,
+                #     n_std=1,
+                #     alpha=0.3,
+                #     facecolor='C1',
+                #     edgecolor=None,
+                # )
+                # p4 = self.confidence_ellipse_smefit(
+                #     coeff1_values,
+                #     coeff2_values,
+                #     ax,
+                #     n_std=1,
+                #     alpha=1,
+                #     edgecolor='C1',
+                # )
 
 
+                hndls.append((p1, p2))
+                # hndls.append((p3, p4))
+
+            ax.scatter(
+                np.mean(coeff1_values, axis=0),
+                np.mean(coeff2_values, axis=0),
+                c=colors[i],
+                s=3,
+            )
 
 
             plt.xlabel(ax_labels[0], fontsize=33)
             plt.ylabel(ax_labels[1], fontsize=33)
             plt.tick_params(which="both", direction="in", labelsize=22)
 
-
-
-        #plt.xlim(-0.06, 0.06)
-        #plt.ylim((-0.5, 0.5))
-
         ax.scatter(0, 0, c="k", marker="s", s=10, zorder=10)
-        # ax.legend(
-        #     loc="upper left", handles=hndls, labels=labels, frameon=False, prop={"size": 15}
-        # )
+
+        if labels is not None:
+            ax.legend(
+                loc="upper right", handles=hndls, labels=labels, frameon=False, prop={"size": 15}
+            )
         #plt.title(r"${\rm 68\%\ Confidence\ Level\ Bounds}$", fontsize=15)
 
 
