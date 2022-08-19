rotation fix in ellipse plotter
Former-commit-id: 2363892c022bcb588ddee582a9932dcdcaccc7d1 [formerly a771dcc1cb9afce1e0a408fb7cdbefbdfccb18ae]
Former-commit-id: 62ef39d8b6136582264e4158974fe37d3188fbff

code/plotting/ellipse_plotter_new.py
@@ -83,7 +83,19 @@ class EllipsePlotter:
         return ax.add_patch(ellipse)
 
     @staticmethod
-    def confidence_ellipse(x, y, ax, kde=False, facecolor="none", **kwargs):
+    def confidence_contour_kde(x, y, ax, hndls, facecolor="none"):
+
+        sns.kdeplot(x, y, levels=[0.05, 1.0], bw_adjust=1.2, ax=ax, fill=True, alpha=0.3, color=facecolor)
+        sns.kdeplot(x, y, levels=[0.05], bw_adjust=1.2, ax=ax, alpha=1, color=facecolor)
+        #ax.scatter(x, y, s=4, alpha=0.2)
+        hndls.append((mpatches.Patch(ec=facecolor, fc=facecolor, fill=True, alpha=0.3),
+                      mpatches.Patch(ec=facecolor, fc=facecolor, fill=False, alpha=1.0)))
+
+        return hndls
+
+
+    @staticmethod
+    def confidence_ellipse(x, y, ax, facecolor="none", **kwargs):
         """
         Create a plot of the covariance confidence ellipse of `x` and `y`
         Parameters
@@ -112,13 +124,16 @@ class EllipsePlotter:
 
         eig_vec_max = eig_vec[:, np.argmax(eig_val)]
 
-        cos_th = eig_vec[np.argmax(eig_val), 0] / np.linalg.norm(eig_vec_max)
-        inclination = np.arccos(cos_th)
+        cos_th = eig_vec[0, np.argmax(eig_val)] / np.linalg.norm(eig_vec_max)
+        if eig_vec_max[1] > 0:
+            inclination = np.arccos(cos_th)
+        else:
+            inclination = - np.arccos(cos_th)
 
         eigval_sort = np.sort(eig_val)
 
-        ell_radius_x = np.sqrt(2.3 * eigval_sort[-1])
-        ell_radius_y = np.sqrt(2.3 * eigval_sort[-2])
+        ell_radius_x = np.sqrt(5.991 * eigval_sort[-1])
+        ell_radius_y = np.sqrt(5.991 * eigval_sort[-2])
 
         ellipse = Ellipse(
             (0, 0),
@@ -128,22 +143,15 @@ class EllipsePlotter:
             **kwargs
         )
 
-        if kde:
-            sns.kdeplot(x, y, levels=[0.32, 1.0], bw_adjust=1.2, ax=ax, fill=True, alpha=0.3, color=facecolor)
-            sns.kdeplot(x, y, levels=[0.32], bw_adjust=1.2, ax=ax, alpha=1, color=facecolor)
-            ax.scatter(x, y, s=4, alpha=0.2)
-
-
-        mean_x = np.mean(x)
-        mean_y = np.mean(y)
+        mean_x = np.median(x)
+        mean_y = np.median(y)
 
         transf = (transforms.Affine2D().rotate(inclination).translate(mean_x, mean_y))
 
         ellipse.set_transform(transf + ax.transData)
 
-        ax.add_patch(ellipse)
+        return ax.add_patch(ellipse), inclination
 
-        return ax.add_patch(ellipse)
 
     def plot(self, ax, dfs, ax_labels, coeff1, coeff2, kde=None, labels=None):
         """
@@ -167,23 +175,43 @@ class EllipsePlotter:
 
             if coeff1_values is not None and coeff2_values is not None:
 
-                p1 = self.confidence_ellipse(
-                    coeff1_values,
-                    coeff2_values,
-                    ax,
-                    alpha=1,
-                    edgecolor=colors[i]
-                )
+                if not kde:
 
-                p2 = self.confidence_ellipse(
-                    coeff1_values,
-                    coeff2_values,
-                    ax,
-                    alpha=0.3,
-                    facecolor=colors[i],
-                    edgecolor=None,
-                    kde=kde[i]
-                )
+                    p1, inc = self.confidence_ellipse(
+                        coeff1_values,
+                        coeff2_values,
+                        ax,
+                        alpha=1,
+                        edgecolor=colors[i],
+                    )
+
+                    p2, _ = self.confidence_ellipse(
+                        coeff1_values,
+                        coeff2_values,
+                        ax,
+                        alpha=0.3,
+                        facecolor=colors[i],
+                        edgecolor=None,
+                    )
+
+                    ax.scatter(
+                        np.mean(coeff1_values, axis=0),
+                        np.mean(coeff2_values, axis=0),
+                        c=colors[i],
+                        s=3,
+                    )
+
+
+
+                    hndls.append((p1, p2))
+
+                else:
+                    hndls = self.confidence_contour_kde(coeff1_values,
+                        coeff2_values,
+                        ax,
+                        hndls,
+                        facecolor=colors[i])
+
 
                 # smefit
 
@@ -206,29 +234,22 @@ class EllipsePlotter:
                 # )
 
 
-                hndls.append((p1, p2))
+
                 # hndls.append((p3, p4))
 
-            ax.scatter(
-                np.mean(coeff1_values, axis=0),
-                np.mean(coeff2_values, axis=0),
-                c=colors[i],
-                s=3,
-            )
 
-
-            plt.xlabel(ax_labels[0], fontsize=33)
-            plt.ylabel(ax_labels[1], fontsize=33)
+            plt.xlabel(ax_labels[0])
+            plt.ylabel(ax_labels[1])
             plt.tick_params(which="both", direction="in", labelsize=22)
 
         ax.scatter(0, 0, c="k", marker="s", s=10, zorder=10)
 
         if labels is not None:
             ax.legend(
-                loc="upper right", handles=hndls, labels=labels, frameon=False, prop={"size": 15}
+                loc="lower left", handles=hndls, labels=labels, frameon=False, prop={"size": 26}
             )
-        #plt.title(r"${\rm 68\%\ Confidence\ Level\ Bounds}$", fontsize=15)
+        else:
+            return hndls
 
+        #plt.title(r"${\rm 68\%\ Confidence\ Level\ Bounds}$", fontsize=20)
 
-        return ax, hndls
-        #plt.savefig('/data/theorie/jthoeve/ML4EFT_jan/ML4EFT/plots/2022/15_07/ellipse_zh_llbb_68.pdf')

code/src/quad_clas/core/th_predictions.py
@@ -155,8 +155,8 @@ class TheoryPred:
             dsigma_dx_c1 = np.array([dsigma_dx(*row[features], [-10, 0], "lin") for _, row
                                      in events.iterrows()])
 
-            dsigma_dx_c2 = np.array(
-                [dsigma_dx(*row[features], [0, 10], "lin") for _, row in events.iterrows()])
+            # dsigma_dx_c2 = np.array(
+            #     [dsigma_dx(*row[features], [0, 10], "lin") for _, row in events.iterrows()])
 
             dsigma_dx_c1_quad = np.array(
                 [dsigma_dx(*row[features], [-10, 0], "quad") for _, row in events.iterrows()])
@@ -166,18 +166,15 @@ class TheoryPred:
 
             dsigma_dx_c1_lin_coef = (dsigma_dx_c1 - dsigma_dx_sm) / (-10)
 
-            dsigma_dx_c2_lin_coef = (dsigma_dx_c2 - dsigma_dx_sm) / 10
+            #dsigma_dx_c2_lin_coef = (dsigma_dx_c2 - dsigma_dx_sm) / 10
 
             dsigma_dx_c1_quad_coef = (dsigma_dx_c1_quad - dsigma_dx_sm) / 100
 
             dsigma_dx_c2_quad_coef = (dsigma_dx_c2_quad - dsigma_dx_sm) / 100
 
-        dsigma_dx = {'sm': dsigma_dx_sm,
-                     'lin': {optimizer.th_pred.c_names[0]: dsigma_dx_c1_lin_coef,
-                             optimizer.th_pred.c_names[1]: dsigma_dx_c2_lin_coef},
-                     'quad': {optimizer.th_pred.c_names[0]: dsigma_dx_c1_quad_coef,
-                             optimizer.th_pred.c_names[1]: dsigma_dx_c2_quad_coef}
-                     }
+            dsigma_dx = {'sm': dsigma_dx_sm,
+                         'lin': {'ctGRe': dsigma_dx_c1_lin_coef},
+                         'quad': {'ctGRe_ctGRe': dsigma_dx_c1_quad_coef, 'ctu1_ctu1': dsigma_dx_c2_quad_coef}}
 
         return dsigma_dx
 

code/src/quad_clas/limits/optimize_ns.py
@@ -281,21 +281,32 @@ class Optimize:
 
     def log_like_truth(self, cube):
 
-        #self.cube_to_dict(cube)
-        for i, c_name in enumerate(self.th_pred.th_dict['lin'].keys()):
-            self.param_names[c_name] = cube[i]
+        self.cube_to_dict(cube)
 
-        # compute inclusive and differential xsec at cube
-        sigma = self.th_pred.th_dict['sm']
-        dsigma_dx = copy.deepcopy(self.dsigma_dx['sm'])
+        # compute inclusive xsec at cube
+        sigma = 0
+        sigma += self.th_pred.th_dict['sm']
 
-        for c_name, sigma_i in self.th_pred.th_dict['lin'].items():
-            sigma += self.param_names[c_name] * sigma_i
-            dsigma_dx += self.param_names[c_name] * self.dsigma_dx['lin'][c_name]
+        dsigma_dx = 0
+        dsigma_dx += self.dsigma_dx['sm']
 
-        for c_name, sigma_i in self.th_pred.th_dict['quad'].items():
-            sigma += self.param_names[c_name] ** 2 * sigma_i
-            dsigma_dx += self.param_names[c_name] ** 2 * self.dsigma_dx['quad'][c_name]
+        lin_models = self.th_pred.th_dict['lin']
+        for c_name, c_val in self.param_names.items():
+
+            # check if c_name has linear sensitivity
+            if c_name in lin_models:
+                sigma += c_val * self.th_pred.th_dict['lin'][c_name]
+                dsigma_dx += c_val * self.dsigma_dx['lin'][c_name]
+
+
+        if 'quad' in self.th_pred.th_dict:
+
+            quad_pred = self.th_pred.th_dict['quad']
+            for (c1, c2) in itertools.product(self.param_names.keys(), repeat=2):
+                c_name = '{}_{}'.format(c1, c2)
+                if c_name in quad_pred:
+                    sigma += self.param_names[c1] * self.param_names[c2] * quad_pred['{}_{}'.format(c1, c2)]
+                    dsigma_dx += self.param_names[c1] * self.param_names[c2] * self.dsigma_dx['quad']['{}_{}'.format(c1, c2)]
 
         nu = sigma * self.lumi
         log_like = -nu + np.sum(np.log(dsigma_dx / self.dsigma_dx['sm']))