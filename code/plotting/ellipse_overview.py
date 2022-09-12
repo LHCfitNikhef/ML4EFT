90ed3390f7dee4ac263a1ee0a9332d8016957b08
Jaco ter Hoeve <jthoeve@stbc-i2.nikhef.nl>
2022-09-12 15:03:50 +0200

    contour overview plot now shares x and y axis in marginal plot
    Former-commit-id: ac0bee3bdcc2307e3aeb5173c1951c74c0c6086b [formerly 954d5f5ad1336c8bf2c1e03f80f72c5da2ee3b31]
    Former-commit-id: 6b962f0cafba07ea86ee6fe8af2abdd424dcdd84

--- code/plotting/ellipse_overview.py
+++ code/plotting/ellipse_overview.py
@@ -57,13 +57,13 @@ nn_glob_quad_path = '/data/theorie/jthoeve/ns_samples/zh_llbb/nn_glob/posterior.
 nn_glob_ptz_quad_path = '/data/theorie/jthoeve/ns_samples/zh_llbb/nn_glob_pt_z_quad/posterior.json'
 binned_glob_quad_path = '/data/theorie/jthoeve/ns_samples/zh_llbb/binned_pt_z_quad/posterior.json'
 
-paths_plot_0 = [nn_glob_ptz_quad_path, nn_glob_quad_path]
+paths_plot_0 = [binned_glob_quad_path, nn_glob_ptz_quad_path, nn_glob_quad_path]
 paths_plot_1 = [post_path_binned_quad, post_path_nn_quad, post_path_nn_quad_7_feat]
 paths_plot_2 = [post_path_binned_lin, post_path_nn_lin]
 paths_plot_3 = [post_path_binned_quad, post_path_nn_quad]
 paths_plot_4 = [post_path_binned_lin, post_path_binned_quad, post_path_nn_lin, post_path_nn_quad]
 
-labels_0 = [ r"$\mathrm{ML}\;\mathrm{model}\;(p_T^Z)$", r"$\mathrm{ML}\;\mathrm{model}\;(7\;\mathrm{features})$", r'$\mathrm{SM}$']
+labels_0 = [r"$p_T^Z\in[75, 150, 250, 400, \infty)\:\mathrm{GeV}$", r"$\mathrm{ML}\;\mathrm{model}\;(p_T^Z)$", r"$\mathrm{ML}\;\mathrm{model}\;(7\;\mathrm{features})$", r'$\mathrm{SM}$']
 labels_1 = [r"$p_T^Z\in[75, 150, 250, 400, \infty)\:\mathrm{GeV}:\mathrm{Quadratic}$", r"$\mathrm{ML}\;\mathrm{model}\;(p_T^Z):\mathrm{Quadratic}$",
           r"$\mathrm{ML}\;\mathrm{model}\;(7\;\mathrm{features}):\mathrm{Quadratic}$"]
 
@@ -83,15 +83,21 @@ def ellipse_overview(coeff_dict, labels, paths):
 
     fig = plt.figure(figsize=(n_cols * 4, n_rows * 4))
 
+    grid = plt.GridSpec(n_rows, n_cols, hspace=0.1, wspace=0.1)
+
     c1_old = "cHu"
-    i = n_cols * n_rows
+
+    row_idx = -1
+    col_idx = -1
     j = 1
     for (c1, c2) in itertools.combinations(coeff_dict.keys(), 2):
         if c1 != c1_old:
-            i -= j
+            row_idx += -1
+            col_idx = -1 - j
             j += 1
             c1_old = c1
-        ax = plt.subplot(n_rows, n_cols, i)
+
+        ax = fig.add_subplot(grid[row_idx, col_idx])
 
         plotter = EllipsePlotter()
 
@@ -101,21 +107,36 @@ def ellipse_overview(coeff_dict, labels, paths):
 
         hndls = plotter.plot(ax, dfs, coeff1=c2, coeff2=c1,
                                  ax_labels=[coeff_dict[c2], coeff_dict[c1]],  kde=True)
-        if j!=1:
-            ax.set_xlabel('')
-        if not (i == n_cols * n_rows + 1 - j * n_cols):
-            ax.set_ylabel('')
+        if row_idx != -1:
+            ax.set(xlabel=None)
+            ax.tick_params(
+                axis='x',  # changes apply to the x-axis
+                which='both',  # both major and minor ticks are affected
+                labelbottom=False)
+        if col_idx != -n_cols:
+            ax.set(ylabel=None)
+            ax.tick_params(
+                axis='y',  # changes apply to the y-axis
+                which='both',  # both major and minor ticks are affected
+                labelleft=False)
 
-        i -= 1
+        col_idx -= 1
 
-    legend = fig.legend(
-            labels=labels, handles=hndls,
-            loc='upper center', frameon=True, fontsize=26,
-    title=r"$\mathrm{Marginalised}\:95\:\%\:\mathrm{C.L.\:interval}\;\mathcal{O}\left(\Lambda^{-4}\right)$")
+    legend = ax.legend(
+        labels=labels, handles=hndls,
+        bbox_to_anchor=(1, 1),
+        loc='upper left', frameon=False, fontsize=24,
+        handlelength=1,
+        borderpad=0.5,
+        handletextpad=1,
+        title_fontsize=24)
+
+    fig.suptitle(r"$\mathrm{Marginalised}\:95\:\%\:\mathrm{C.L.\:intervals},\;\mathcal{O}\left(\Lambda^{-4}\right)\mathrm{at\:}L=300\:\mathrm{fb}^{-1}$", y=0.92)
+
 
     bbox = legend.get_window_extent(fig.canvas.get_renderer()).transformed(fig.transFigure.inverted())
 
-    plt.tight_layout()
+
 
     return fig
 
@@ -126,7 +147,7 @@ fig_0 = ellipse_overview(coeff_dict, labels_0, paths_plot_0)
 # fig_4 = ellipse_overview(coeff_dict, labels_4, paths_plot_4)
 
 #fig_1.savefig('/data/theorie/jthoeve/ML4EFT_jan/ML4EFT/plots/2022/18_08/zh_particle_quad_kde_7_feat.pdf')
-fig_0.savefig('/data/theorie/jthoeve/ML4EFT_jan/ML4EFT/plots/2022/06_09/zh_llbb_quad_glob_binned_vs_nn_ptz_test.pdf')
+fig_0.savefig('/data/theorie/jthoeve/ML4EFT_jan/ML4EFT/plots/2022/12_09/zh_llbb_quad_glob_binned_vs_nn_test.pdf')
 #fig_2.savefig('/data/theorie/jthoeve/ML4EFT_jan/ML4EFT/plots/2022/18_08/zh_particle_lin.pdf')
 #fig_3.savefig('/data/theorie/jthoeve/ML4EFT_jan/ML4EFT/plots/2022/18_08/zh_particle_quad_kde.pdf')
 # fig_4.savefig('/data/theorie/jthoeve/ML4EFT_jan/ML4EFT/plots/2022/18_08/test_overview_4.pdf')
