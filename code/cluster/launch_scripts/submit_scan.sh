9a109359de2ebdcccfeb0763e717189f14cfdfb2
jacoterh <54140851+jacoterh@users.noreply.github.com>
2022-10-14 15:09:23 +0200

    cleaning up repo
    Former-commit-id: dbc9ebb65a7a7988909228be7b39bdc4488b2769 [formerly c5c05e8ba49227f9835e95759e1825b741555bc1] [formerly b5b4126f7a97284be350c07dd7d7e6f8774bd054 [formerly 4ecea89a8f12bd9f9ddf15b81d4b05f4c076532b]]
    Former-commit-id: 7e3593e91748d9a118b5e518c8c0e45e62121642
    Former-commit-id: a829d1225fbcbdcb19dd8667d2dca34507b300a3
    Former-commit-id: 01a1b0121d535662e77581780c994b28df2780cd

--- code/cluster/launch_scripts/submit_scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# script to submit jobs to the smefit queue
-
-pbs_file=/data/theorie/jthoeve/ML4EFT_v2/cluster/launch_scripts/scan.pbs
-for mc_run in `seq 1 1`; do
-    qsub -q smefit -W group_list=smefit -v ARG=$mc_run $pbs_file
-done
