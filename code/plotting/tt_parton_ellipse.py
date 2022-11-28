961a82a51f196908e162c899d860ad1399ae716a
pim <pimnelson@hotmail.com>
2022-11-28 16:06:13 +0100

    "ellips plotting code"

--- code/plotting/tt_parton_ellipse.py
+++ code/plotting/tt_parton_ellipse.py
@@ -18,8 +18,8 @@ rc('text', usetex=True)
 coeff_dict = {"ctGRe": r'$c_{tG}$', "ctu8": r'$c_{tu}^{(8)}$'}
 
 # LINEAR
-samples_nn_lin_mtt1 = '/data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe/posteriors/2_feat_ctu8_ctGRe_binned/posterior.json'
-samples_nn_lin_mtt2 = '//data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe/posteriors/2_feat_ctu8_ctGRe_binned1.2/posterior.json'
+samples_nn_lin_mtt1 = '/data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe/posteriors/binned_ctu8_ctGRe_quad_fine_binned/posterior.json'
+# samples_nn_lin_mtt2 = '/data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe/posteriors/2_feat_ctu8_ctGRe_binned1.2/posterior.json'
 # samples_nn_lin_mtt3 = '/data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe/posteriors/nn_ctu8_ctGRe_subproj/posterior.json'
 
 # samples_binned_lin_mtt_y_c = '/data/theorie/jthoeve/ns_samples/tt_parton/binned_glob_lin_mtt_y_c/posterior.json'
@@ -46,14 +46,14 @@ samples_nn_lin_mtt2 = '//data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe
 # PLOT 1
 
 df_plot_1 = [Analyse.posterior_loader(samples_nn_lin_mtt1)
-            ,Analyse.posterior_loader(samples_nn_lin_mtt2)
+            # ,Analyse.posterior_loader(samples_nn_lin_mtt2)
             # ,Analyse.posterior_loader(samples_nn_lin_mtt3)
             # ,
             #  Analyse.posterior_loader(samples_nn_lin_mtt_y)
              ]
 
 labels_plot_1 = [r'$\mathrm{Binning}\:1\;(m_{t\bar{t}},y_{t\bar{t}})$'
-                ,r'$\mathrm{Binning}\:1\;(m_{t\bar{t}},y_{t\bar{t}})$'
+                # ,r'$\mathrm{Binning}\:1\;(m_{t\bar{t}},y_{t\bar{t}})$'
                 # , 'nn'
                 #  r'$\mathrm{Unbinned\;exact}\;(m_{t\bar{t}}, y_{t\bar{t}})$',
                 #  r'$\mathrm{Unbinned\;ML}\;(m_{t\bar{t}}, y_{t\bar{t}})$',
@@ -109,7 +109,7 @@ labels = [labels_plot_1
  ]
 
 titles = [
-    r"$95\:\%\:\mathrm{C.L.\:intervals},\;\mathcal{O}\left(\Lambda^{-2}\right)\mathrm{at\:}\mathcal{L}=2571\:\mathrm{fb}^{-1}$"
+    r"$95\:\%\:\mathrm{C.L.\:intervals},\;\mathcal{O}\left(\Lambda^{-2}\right)\mathrm{at\:}\mathcal{L}=2571\:\mathrm{fb}^{-1}, quad$"
     # ,r"$95\:\%\:\mathrm{C.L.\:intervals},\;\mathcal{O}\left(\Lambda^{-4}\right)\mathrm{at\:}\mathcal{L}=300\:\mathrm{fb}^{-1}$",
     # r"$95\:\%\:\mathrm{C.L.\:intervals},\;\mathcal{O}\left(\Lambda^{-2}\right)\mathrm{at\:}\mathcal{L}=300\:\mathrm{fb}^{-1}$",
     # r"$95\:\%\:\mathrm{C.L.\:intervals},\;\mathcal{O}\left(\Lambda^{-4}\right)\mathrm{at\:}\mathcal{L}=300\:\mathrm{fb}^{-1}$"
@@ -153,4 +153,4 @@ for i, df in enumerate(dfs):
 
 grid.tight_layout(fig)
 
-fig.savefig('/data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe/plots/2_feat_ctu8_ctGRe_binned_comparison')
+fig.savefig('/data/theorie/pherbsch/ML4EFT/results/subproj_ctu8_ctGRe/plots/2_feat_ctu8_ctGRe_binned_quad')
