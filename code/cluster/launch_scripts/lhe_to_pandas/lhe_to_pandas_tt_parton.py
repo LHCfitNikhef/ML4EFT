6d627480df3fa88ea3e02f2157dda51158edde95
jacoterh <54140851+jacoterh@users.noreply.github.com>
2023-02-01 10:55:08 +0100

    updating to latest lhe readers standards

--- code/cluster/launch_scripts/lhe_to_pandas/lhe_to_pandas_tt_parton.py
+++ code/cluster/launch_scripts/lhe_to_pandas/lhe_to_pandas_tt_parton.py
@@ -5,13 +5,13 @@ import sys, os
 import subprocess
 import ml4eft.preproc.lhe_reader as lhe
 
-event_dir = sys.argv[1]
-process = sys.argv[2]
-save_root = sys.argv[3]
+# event_dir = sys.argv[1]
+# process = sys.argv[2]
+# save_root = sys.argv[3]
 
 def lhe_to_pandas(path_to_lhe):
 
-    lhe_init = pylhe.readLHEInit(path_to_lhe)
+    lhe_init = pylhe.read_lhe_init(path_to_lhe)
 
     xsec = 0
     for process in lhe_init['procInfo']:
@@ -19,7 +19,7 @@ def lhe_to_pandas(path_to_lhe):
 
     events = []
 
-    for e in pylhe.readLHE(path_to_lhe):
+    for e in pylhe.read_lhe(path_to_lhe):
         # create particle instances
         for part in e.particles:
             if part.id == 6: # t
@@ -45,49 +45,59 @@ def lhe_to_pandas(path_to_lhe):
 
     return df
 
-rep_nr = 0
-if os.path.basename(event_dir).startswith(process) is True:
+df = lhe_to_pandas('/Users/jaco/Documents/PhD/MG5_aMC_v3_4_1/bin/positivity_test/Events/run_03/unweighted_events.lhe')
+df2 = lhe_to_pandas('/Users/jaco/Documents/PhD/MG5_aMC_v3_4_1/bin/positivity_test/Events/run_02/unweighted_events.lhe')
+import matplotlib.pyplot as plt
 
-    job_dirs = os.listdir(event_dir)
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.hist(df.iloc[1:, :]['m_tt'])
+ax.hist(df2.iloc[1:, :]['m_tt'])
+ax.set_yscale('log')
+fig.savefig('/Users/jaco/Documents/ML4EFT/plots/test.pdf')
 
-    # loop over all jobs
-    for job in job_dirs:
-
-        run_path = os.path.join(event_dir, job, 'Events')
-
-        # exclude non-job directories
-        if not job.startswith('job'):
-            continue
-
-        # check for successful run
-        if len(os.listdir(run_path)) == 0:
-            continue
-        else:
-            run_nrs = [int(runs.split("_", 1)[-1]) for runs in os.listdir(run_path)]
-            run_nr_max = max(run_nrs)
-
-        # build lhe path
-        if run_nr_max < 10:
-            lhe_path = os.path.join(run_path, 'run_0{}'.format(run_nr_max), 'unweighted_events.lhe')
-        else:
-            lhe_path = os.path.join(run_path, 'run_{}'.format(run_nr_max), 'unweighted_events.lhe')
-
-        # extract .gz file if necessary
-        if os.path.isfile('{}.gz'.format(lhe_path)):
-            subprocess.run("gunzip {}.gz".format(lhe_path), shell=True)
-
-        # feed lhe file to pandas converter
-        df = lhe_to_pandas(lhe_path)
-
-        event_name = os.path.basename(event_dir)
-        save_dir = os.path.join(save_root, event_name)
-
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-
-        df.to_pickle(os.path.join(save_dir, "events_{}.pkl.gz".format(rep_nr)), compression="infer")
-
-        rep_nr += 1
-else:
-    sys.exit()
+# rep_nr = 0
+# if os.path.basename(event_dir).startswith(process) is True:
+#
+#     job_dirs = os.listdir(event_dir)
+#
+#     # loop over all jobs
+#     for job in job_dirs:
+#
+#         run_path = os.path.join(event_dir, job, 'Events')
+#
+#         # exclude non-job directories
+#         if not job.startswith('job'):
+#             continue
+#
+#         # check for successful run
+#         if len(os.listdir(run_path)) == 0:
+#             continue
+#         else:
+#             run_nrs = [int(runs.split("_", 1)[-1]) for runs in os.listdir(run_path)]
+#             run_nr_max = max(run_nrs)
+#
+#         # build lhe path
+#         if run_nr_max < 10:
+#             lhe_path = os.path.join(run_path, 'run_0{}'.format(run_nr_max), 'unweighted_events.lhe')
+#         else:
+#             lhe_path = os.path.join(run_path, 'run_{}'.format(run_nr_max), 'unweighted_events.lhe')
+#
+#         # extract .gz file if necessary
+#         if os.path.isfile('{}.gz'.format(lhe_path)):
+#             subprocess.run("gunzip {}.gz".format(lhe_path), shell=True)
+#
+#         # feed lhe file to pandas converter
+#         df = lhe_to_pandas(lhe_path)
+#
+#         event_name = os.path.basename(event_dir)
+#         save_dir = os.path.join(save_root, event_name)
+#
+#         if not os.path.exists(save_dir):
+#             os.makedirs(save_dir)
+#
+#         df.to_pickle(os.path.join(save_dir, "events_{}.pkl.gz".format(rep_nr)), compression="infer")
+#
+#         rep_nr += 1
+# else:
+#     sys.exit()
 
